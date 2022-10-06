d0ecbf2500f20d2f0f9735796c0134724d431875
Richa Agrawal <richa.agrawal@juspay.in>
2022-10-06 17:34:24 +0530

    jhbjbf

---
+++ order-status-api/shell-request.sh
@@ -0,0 +1,4 @@
+curl --location --request GET 'https://api.juspay.in/orders/<order_id>' \
+--header 'x-merchantid: your_merchant_id' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'Authorization: Basic base_64_encoded_api_key==' 
