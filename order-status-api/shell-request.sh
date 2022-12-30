b42c7cc0102aa1292e45f399d1e28e1228b943c6
Richa Agrawal <richa.agrawal@juspay.in>
2022-12-30 13:26:57 +0530

    F1PSS-265-JUSPAY-SDK-INTEGRATION-DEMO

--- order-status-api/shell-request.sh
+++ order-status-api/shell-request.sh
@@ -1,4 +1,5 @@
 curl --location --request GET 'https://api.juspay.in/orders/<order_id>' \
 --header 'x-merchantid: your_merchant_id' \
+--header 'version: <current date>' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: Basic base_64_encoded_api_key==' 
