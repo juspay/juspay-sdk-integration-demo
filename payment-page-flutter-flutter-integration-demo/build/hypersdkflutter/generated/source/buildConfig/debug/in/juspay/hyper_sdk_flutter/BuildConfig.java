966200f2580600f7424aa8790ad52300c24442c8
Arbinda Kumar Prasad <arbinda.prasad@juspay.in>
2023-02-06 15:35:23 +0530

    Switched to hypersdkflutter from hypersdk

---
+++ payment-page-flutter-flutter-integration-demo/build/hypersdkflutter/generated/source/buildConfig/debug/in/juspay/hyper_sdk_flutter/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package in.juspay.hyper_sdk_flutter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "in.juspay.hyper_sdk_flutter";
+  public static final String BUILD_TYPE = "debug";
+}
