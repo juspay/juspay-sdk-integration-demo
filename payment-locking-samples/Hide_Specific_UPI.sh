c9708a07b8a56b076e062d967ddb3e904a010871
Arbinda Kumar Prasad <arbinda.prasad@juspay.in>
2023-02-15 18:54:24 +0530

    payment-locking-samples

--- payment-locking-samples/Hide_Specific_UPI.sh
+++ payment-locking-samples/Hide_Specific_UPI.sh
@@ -1,18 +1,20 @@
-url --location --request POST 'https://api.juspay.in/session' \
-    --header 'x-merchantid: yourMerchantId' \
-    --header 'Authorization: Basic base64encodedkey==' \
-    --header '{
-   "order_id": "R8205947560",
-   "first_name": "John",
-   "last_name": "Wick",
-   "mobile_number": "9592329220",
-   "email_address": "test007@gmail.com",
-   "customer_id": "9592329220",
-   "timestamp": "1611559686153",
-   "merchant_id": "abcd",
-   "amount": "1.00",
-   "currency": "INR",
-   "payment_filter": {
+curl --location --request POST 'https://api.juspay.in/session' \
+--header 'Authorization: Basic base_64_encoded_api_key==' \
+--header 'x-merchantid: your_merchant_id' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "order_id": "testing-order-one",
+    "amount": "1.0",
+    "customer_id": "testing-customer-one",
+    "customer_email": "test@mail.com",
+    "customer_phone": "9876543210",
+    "payment_page_client_id": "your_client_id",
+    "action": "paymentPage",
+    "return_url": "https://shop.merchant.com",
+    "description": "Complete your payment",
+    "first_name": "John",
+    "last_name": "wick",
+    "payment_filter": {
        "allowDefaultOptions": true,
        "options": [{
                "paymentMethodType": "UPI",
