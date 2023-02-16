50e9e8f05ccd945174e243828f91f4d88f42791d
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-02-16 23:59:35 +0530

    misc code snippets updates

--- misc/UUID-objC.sh
+++ misc/UUID-objC.sh
@@ -1,4 +1,3 @@
 
-# Sample snippets to generate uuid-v4 string. Used in HyperSDK as [requestId]
-
+# Please scroll down for a sample snippets to generate uuid-v4 string. Used in HyperSDK as [requestId]
 NSUUID.UUID.UUIDString;
