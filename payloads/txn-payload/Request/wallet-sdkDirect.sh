8eed48da048a21edcb9df2e54cf86f529de4666e
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-02-17 14:18:06 +0530

    wallets code update

---
+++ payloads/txn-payload/Request/wallet-sdkDirect.sh
@@ -0,0 +1,16 @@
+{
+  "requestId": "d3aec4b3-3cf5-45a1-b999-9b06d94af678",
+  "service": "in.juspay.hyperapi",
+  "payload": {
+    "action": "walletTxn",
+    "orderId": "hyperorder45314",
+    "paymentMethod": "GOOGLEPAY",
+    "sdkPresent": "ANDROID_GOOGLEPAY",
+    "directWalletToken": "tkn_nvtQf8DP1A2oGoZu",
+    "endUrls": [
+      "https:\\/\\/shop\\.merchant\\.com\\/payments\\/handleResponse.*"
+    ],
+    "clientAuthToken": "tkn_0e37edc631d647fdb606ab48ccfc4213",
+    "showLoader": true
+  }
+}
