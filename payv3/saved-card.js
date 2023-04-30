11a0110f49c3b01385df59f903eab0a9bae3b337
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-04-30 22:04:25 +0530

    payv3

---
+++ payv3/saved-card.js
@@ -0,0 +1,21 @@
+<form class="juspay_inline_form" id="payment_form1">
+    <input type="hidden" class="merchant_id" value="guest"/>
+    <input type="hidden" class="order_id" value="guest_order1"/>
+    <input type="hidden" class="card_token"/>
+    <input type="hidden" class="card_isin"/>
+    <div class="security_code_div"></div>
+    <input type="radio" class="auth_type" value="" name="auth_type"> Verify with Secure Password
+    <input type="hidden" class="redirect" value="true"/>
+</form>
+<form class="juspay_inline_form" id="payment_form2">
+    <input type="hidden" class="merchant_id" value="guest"/>
+    <input type="hidden" class="order_id" value="guest_order2"/>
+    <input type="hidden" class="card_token"/>
+    <input type="hidden" class="card_isin"/>
+    <div class="security_code_div"></div>
+    <input type="radio" class="auth_type" value="" name="auth_type"> Verify with Secure Password
+    <input type="hidden" class="redirect" value="true"/>
+    <input type="hidden" class="offers" value="62dc1489-6d961ef28bc02617, de2f321f-c4ce-478a-bcca"/> 
+    <input type="hidden" class="payment_channel" value="WEB"/>
+    <button type="submit" class="make_payment">Pay</button>
+</form>
