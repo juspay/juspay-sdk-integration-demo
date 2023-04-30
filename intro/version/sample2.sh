c0c00f8469bda337a0f3df1895b6704ef3d6255f
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-04-30 22:07:44 +0530

    intro

---
+++ intro/version/sample2.sh
@@ -0,0 +1,4 @@
+curl --location --request POST 'https://dashboard.sandbox.juspay.in/orders' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'version: 2021-03-05' \
+--header 'Authorization: Basic QkIwQjMyODuwdqdg8eq0NzBBMUE2Og=='
