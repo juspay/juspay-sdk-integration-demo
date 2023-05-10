63d61970851ad4f30ff4e737130c2d89b82db7f5
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-05-10 16:32:33 +0530

    upi

---
+++ upi/collect.js
@@ -0,0 +1,12 @@
+{
+    "order_id": "test_9069_789",
+    "txn_id": "aman_prod-test_9069_789-9",
+    "txn_uuid": "dev8pyieixvdyldt",
+    "status": "PENDING_VBV",
+    "payment": {
+      "authentication": {
+        "method": "GET",
+        "url": "https://api.juspay.in/pay/start/aman_prod/dev8pyieixvxxxxt"
+      }
+    }
+  }
