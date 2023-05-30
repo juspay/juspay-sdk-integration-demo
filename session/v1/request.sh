9618428f1abbe4fc8d4e630830e9915de86da001
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-05-30 19:45:32 +0530

    session-api-v1

---
+++ session/v1/request.sh
@@ -0,0 +1,13 @@
+{
+  "order_id": "testing-order-one",
+  "amount": "1.0",
+  "customer_id": "testing-customer-one",
+  "customer_email": "test@mail.com",
+  "customer_phone": "9876543210",
+  "payment_page_client_id": "yourClientId",
+  "action": "paymentPage",
+  "return_url": "https://shop.merchant.com",
+  "description": "Complete your payment",
+  "billing_address_first_name": "John",
+  "billing_address_last_name": "wick"
+}
