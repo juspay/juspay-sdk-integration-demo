35ff88e7ff4e8b783a00fa7faa61b778f2b7bfca
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-06-21 16:45:16 +0530

    return-url

--- mandate-registration/request.sh
+++ mandate-registration/request.sh
@@ -9,6 +9,7 @@ curl --location --request POST 'https://api.juspay.in/session' \
     "customer_phone":"9876543210",
     "customer_email":"dummyemail@gmail.com",
     "payment_page_client_id":"picasso",
+    "return_url": "https://shop.merchant.com",
     "action":"paymentPage",
     "options.create_mandate":"OPTIONAL/REQUIRED",
     "mandate.max_amount":"1000.00",
