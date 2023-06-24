7f044060d6e92482490e422c424c9340043034a0
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-06-24 23:53:30 +0530

    gpay

---
+++ orders/refunds/request.sh
@@ -0,0 +1,6 @@
+curl --location --request POST 'https://api.juspay.in/refunds' \
+--header 'Authorization: Basic NTlE*****E4QzE1Og==' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'unique_request_id=refund_122_3472' \
+--data-urlencode 'amount=1' \
+--data-urlencode 'txn_id=merchant_success-JP1636706983-2'
