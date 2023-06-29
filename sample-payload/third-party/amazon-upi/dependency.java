70e5a929d4ec66d68318f4792a77f36e1256c83c
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-06-29 14:34:44 +0530

    amazon-upi

---
+++ sample-payload/third-party/amazon-upi/dependency.java
@@ -0,0 +1,6 @@
+dependencies {
+    implementation 'in.juspay:hyperapayupi:2.1.11'
+    
+    // Make sure to have hypersdk version same as hyperapayupi.
+    implementation 'in.juspay:hypersdk:2.1.11'
+}
---
+++ sample-payload/third-party/amazon-upi/request.sh
@@ -0,0 +1,11 @@
+{
+    "requestId": "7bd92330-2c0f-4dbc-91d0-8f2484910b40",
+    "service": "in.juspay.hyperapi",
+    "payload": {
+        "action": "upiTxn",
+        "orderId": "hyperorder95862",
+        "getAvailableApps": true,
+        "version": “v2”,
+        "clientAuthToken": "tkn_af0a8502674c4d6abe94d33483ad86e3"
+    }
+}
