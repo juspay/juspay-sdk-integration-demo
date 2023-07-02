96384e5f2bf8817276f8afc01b5b0ecf1aeb4cfd
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-07-02 23:43:59 +0530

    capacitor-update

--- ec-headless-capacitor-integration-demo/lib/screens/home.dart
+++ ec-headless-capacitor-integration-demo/lib/screens/home.dart
@@ -1,37 +1,24 @@
-void initiateHyperSDK() async {
-  // Check whether hyperSDK is already initialised
-  if (!await widget.hyperSDK.isInitialised()) {
-    // Getting initiate payload
-    // block:start:get-initiate-payload
-    
-    var initiatePayload = {
-      "requestId": const Uuid().v4(),
-      "service": "in.juspay.hyperapi",
-      "payload": {
-        "action": "initiate",
-        "merchantId": "<merchant-id>",
-        "clientId": "<client-id>",
-        "environment": "production"
-      }
-    };
-    // block:end:get-initiate-payload
 
-    // Calling initiate on hyperSDK instance to boot up payment engine.
-    // block:start:initiate-sdk
+// Getting initiate payload
+// block:start:get-initiate-payload
 
-    await HyperServices.initiate(initiatePayload);
-    // block:end:initiate-sdk
+var initiatePayload = {
+  "requestId": const Uuid().v4(),
+  "service": "in.juspay.hyperapi",
+  "payload": {
+    "action": "initiate",
+    "merchantId": "<merchant-id>",
+    "clientId": "<client-id>",
+    "environment": "production"
   }
-}
+};
+// block:end:get-initiate-payload
 
-// Define handler for inititate callback
-// block:start:initiate-callback-handler
+// Calling initiate on hyperSDK instance to boot up payment engine.
+// block:start:initiate-sdk
 
-void initiateCallbackHandler(MethodCall methodCall) {
-  if (methodCall.method == "initiate_result") {
-    // check initiate result
-  }
-}
-// block:end:initiate-callback-handler
+await HyperServices.initiate(initiatePayload);
+// block:end:initiate-sdk
+
 
 
--- ec-headless-capacitor-integration-demo/lib/screens/payment_page.dart
+++ ec-headless-capacitor-integration-demo/lib/screens/payment_page.dart
@@ -23,101 +23,30 @@
   // block:end:process-sdk
 }
 
-// Define handler for callbacks from hyperSDK
-// block:start:callback-handler
+// Listen to events from HyperSDK
 
-void hyperSDKCallbackHandler(MethodCall methodCall) {
-  switch (methodCall.method) {
-    case "hide_loader":
-      setState(() {
-        showLoader = false;
-      });
+HyperServices.addListener('HyperEvent', async (data) => {
+   var event = data["event"];
+   switch (event) {
+      case "show_loader": {
+         // Show a loader
+      }
       break;
-    case "process_result":
-      var args = {};
-
-      try {
-        args = json.decode(methodCall.arguments);
-      } catch (e) {
-        print(e);
+      case "hide_loader": {
+         // Hide Loader
       }
-
-      var error = args["error"] ?? false;
-
-      var innerPayload = args["payload"] ?? {};
-
-      var status = innerPayload["status"] ?? " ";
-      var pi = innerPayload["paymentInstrument"] ?? " ";
-      var pig = innerPayload["paymentInstrumentGroup"] ?? " ";
-
-      if (!error) {
-        switch (status) {
-          case "charged":
-            {
-              // block:start:check-order-status
-              // Successful Transaction
-              // check order status via S2S API
-              // block:end:check-order-status
-              setState(() {
-                paymentSuccess = true;
-                paymentFailed = false;
-              });
-            }
-            break;
-          case "cod_initiated":
-            {
-              // User opted for cash on delivery option displayed on the checkout screen
-            }
-            break;
-        }
-      } else {
-        var errorCode = args["errorCode"] ?? " ";
-        var errorMessage = args["errorMessage"] ?? " ";
-        switch (status) {
-          case "backpressed":
-            {
-              // user back-pressed from PP without initiating any txn
-              setState(() {
-                paymentFailed = true;
-                paymentSuccess = false;
-              });
-            }
-            break;
-          case "user_aborted":
-            {
-              // user initiated a txn and pressed back
-              // check order status via S2S API
-            }
-            break;
-          case "pending_vbv":
-            {}
-            break;
-          case "authorizing":
-            {
-              // txn in pending state
-              // check order status via S2S API
-            }
-            break;
-          case "authorization_failed":
-            {}
-            break;
-          case "authentication_failed":
-            {}
-            break;
-          case "api_failure":
-            {
-              // txn failed
-              // check order status via S2S API
-            }
-            break;
-          case "new":
-            {
-              // order created but txn failed
-              // check order status via S2S API
-            }
-            break;
-        }
+      break;
+      case "initiate_result": {
+         // Initiate api response
       }
-  }
-}
-// block:end:callback-handler
+      break;
+      case "process_result": {
+         // Process result
+      }
+      break;
+      default:
+         let payload = data["payload"];
+         console.log("process result: ", payload)
+      break;
+   }
+});
