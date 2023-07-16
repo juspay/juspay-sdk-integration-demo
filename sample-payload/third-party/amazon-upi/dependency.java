e35ab2f4ef74b93650d86fece2779ce1e194eb84
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-07-16 13:45:32 +0530

    amazon-upi

--- sample-payload/third-party/amazon-upi/dependency.java
+++ sample-payload/third-party/amazon-upi/dependency.java
@@ -1,6 +1,4 @@
-dependencies {
-    implementation 'in.juspay:hyperapayupi:2.1.13'
-    
-    // Make sure to have hypersdk version same as hyperapayupi.
-    implementation 'in.juspay:hypersdk:2.1.13'
+hyperSdkPlugin {
+    clientId = "<CLIENT_ID>"
+    sdkVersion = "2.1.13"
 }
