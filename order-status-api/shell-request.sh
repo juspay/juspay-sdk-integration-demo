6e5237c8f513a7bdf7c75942d705803c37728134
Arbinda Kumar Prasad <arbinda.prasad@juspay.in>
2023-07-19 11:53:00 +0530

    change: github>juspay>juspay-sdk-integration-demo>payv3-forms>order-status-api/shell-request.sh @ hyper-checkout/android/order-status-api by Arbinda Kumar Prasad

--- order-status-api/shell-request.sh
+++ order-status-api/shell-request.sh
@@ -3,3 +3,6 @@ curl --location --request GET 'https://api.juspay.in/orders/<order_id>' \
 --header 'version: <current date>' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: Basic base_64_encoded_api_key==' 
+
+
+123
