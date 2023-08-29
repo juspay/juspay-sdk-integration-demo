c23cc272594cd414d7509aceb08f3c0a64169fa1
Ankit-Jain11 <122526654+Ankit-Jain11@users.noreply.github.com>
2023-08-29 18:31:54 +0530

    Update request.sh

--- payments/wallet-direct/request.sh
+++ payments/wallet-direct/request.sh
@@ -4,7 +4,7 @@ curl --location 'https://api.juspay.in/txns' \
 --data-urlencode 'order_id=de83e2f6-fa65-44b2-91f2-9af8bbbcc06b' \
 --data-urlencode 'merchant_id=azharamin' \
 --data-urlencode 'payment_method_type=WALLET' \
---data-urlencode 'payment_method=PAYTM' \
+--data-urlencode 'payment_method=GOPAY' \
 --data-urlencode 'redirect_after_payment=true' \
 --data-urlencode 'format=json' \
 --data-urlencode 'direct_wallet_token=tkn_ef1a29396adb4ced9ea1b5a43562ac7b' \
