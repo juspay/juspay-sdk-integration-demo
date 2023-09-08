6cb9dda2a4d78568cdb61e16bf47b15d3c648aa3
Arbinda Kumar Prasad <38696263+ArbindKrPrasad@users.noreply.github.com>
2023-09-08 16:17:58 +0530

    Create webhooks-sample-request.sh

---
+++ webhooks/webhooks-sample-request.sh
@@ -0,0 +1,4 @@
+curl --location --request POST '<merchant-url>' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Basic <Base64_encoded_username:password>' \
+--data <Payload>
