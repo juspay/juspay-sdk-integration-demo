39dca88c2ee986763fa38694f0aa6bcbf68b2444
Ankit-Jain11 <122526654+Ankit-Jain11@users.noreply.github.com>
2023-09-11 13:52:45 +0530

    Update gateway-ref.sh

--- juspay-api-samples/session/gateway-ref.sh
+++ juspay-api-samples/session/gateway-ref.sh
@@ -3,10 +3,10 @@ curl --location --request POST ‘https://api.juspay.in/session’ \
 --header ‘Authorization: Basic base64encodedkey==’ \
 --header ‘Content-Type: application/json’ \
 --data-raw ‘{
-“amount”:“10.00",
+“amount”:“10.00“,
 “order_id”:“yourUniqueOrderId”,
 “customer_id”:“dummyCustId”,
-“customer_phone”:“9876543210",
+“customer_phone”:“9876543210“,
 “customer_email”:“dummyemail@gmail.com”,
 “payment_page_client_id”:“<YOUR_CLIENT_ID>“,
 “action”:“paymentPage”,
