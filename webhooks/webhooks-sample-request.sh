75aba3f2dfaab34725fd28b47123bbbfa5391e1a
Arbinda Kumar Prasad <38696263+ArbindKrPrasad@users.noreply.github.com>
2023-09-20 13:33:09 +0530

    Update webhooks-sample-request.sh

--- webhooks/webhooks-sample-request.sh
+++ webhooks/webhooks-sample-request.sh
@@ -1,3 +1,4 @@
 curl --location --request POST '<merchant-webkook-url>' \
 --header 'Content-Type: application/json' \
 --header 'Authorization: Basic <Base64_encoded_pair_username:password>' \
+--data <Webhook-Payload>
