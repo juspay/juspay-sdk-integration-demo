b14bbd8708efc6c42608b67587066d1245160edd
Vaibhav DSouza <vaibhav.dsouza@juspay.in>
2023-09-27 10:17:43 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference-global>orders/refund-order/request.sh @ paytm-gateway-configuration/docs/refund-order-api by Vaibhav DSouza

--- orders/refund-order/request.sh
+++ orders/refund-order/request.sh
@@ -4,4 +4,4 @@ curl -X POST https://api.juspay.in/orders/1418394476/refunds \
 -H 'Content-Type: application/x-www-form-urlencoded'\
 -H 'x-merchantid: merchant_id'\
 -d "unique_request_id=xyz123" \
--d "amount=100.00"
+-d "amount=1.00"
