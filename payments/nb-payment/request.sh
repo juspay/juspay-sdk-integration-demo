1c3d5c35c40a5c6e4a50d74b41d3e9c0849ec808
Chinthala Shoukthik Sai <chinthala.shoukthiksai@juspay.in>
2023-09-27 16:51:41 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference-global>payments/nb-payment/request.sh @ paytm-gateway-configuration/docs/netbanking-payment by Chinthala Shoukthik Sai

--- payments/nb-payment/request.sh
+++ payments/nb-payment/request.sh
@@ -2,6 +2,6 @@ curl -X POST https://api.juspay.in/txns \
 -d "order_id=:order_id" \
 -d "merchant_id=:merchant_id" \
 -d "payment_method_type=NB" \
--d "payment_method=NB_ICICI" \
+-d "payment_method=NB_BCA" \
 -d "redirect_after_payment=true" \
 -d "format=json"
