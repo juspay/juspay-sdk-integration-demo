3d1c73b77d8e5854728f167962d954fc36d2176f
Deepesh Maheshwari <deepesh.maheshwari@juspay.in>
2023-10-11 12:45:14 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference>upi/collect.sh @ api-reference/docs/upi-collect by Deepesh Maheshwari

--- upi/collect.sh
+++ upi/collect.sh
@@ -1,6 +1,9 @@
-curl -X POST \
-  https://api.juspay.in/txns \
-  -H 'Content-Type: application/x-www-form-urlencoded' \
-  -H 'Postman-Token: 080f8d26-14a0-xxxx-xxxx-c762677c1798' \
-  -H 'cache-control: no-cache' \
-  -d 'order_id=64127725&merchant_id=guest&payment_method_type=UPI&payment_method=UPI&txn_type=UPI_COLLECT&upi_vpa=aman.sharma163%40okicici&format=json&redirect_after_payment=true'
+curl -X POST https://api.juspay.in/txns \
+-d "order_id=:order_id" \
+-d "merchant_id=:merchant_id" \
+-d "payment_method_type=UPI" \
+-d "payment_method=UPI" \
+-d "txn_type=UPI_COLLECT" \
+-d "upi_vpa=:vpa" \
+-d "redirect_after_payment=true" \
+-d "format=json"
