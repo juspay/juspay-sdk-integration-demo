c95c8aec2c08563b645a190e601feedd06175264
Atul Pandey <atul.pandey@juspay.in>
2023-10-11 19:42:39 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference>mandates/create-order/ecr-order.sh @ hyper-checkout/web/registration-api-with-tpv-flow by Atul Pandey

--- mandates/create-order/ecr-order.sh
+++ mandates/create-order/ecr-order.sh
@@ -1,6 +1,6 @@
-curl --location --request POST 'https://sandbox.juspay.in/session' \
+curl --location --request POST 'https://api.juspay.in/session' \
 --header 'Content-Type: application/json' \
---header 'Authorization: Basic MkNFQkU4QjYyQkQ0MDkwOEMzNzNGQUNBRDRGNjA1Og==' \
+--header 'Authorization: Basic base_64_encoded_api_key==' \
 --header 'version: 2023-08-01' \
 --data-raw '{
   "order_id": "order1697020098a",
@@ -8,11 +8,10 @@ curl --location --request POST 'https://sandbox.juspay.in/session' \
   "customer_id": "cth_qYcqE4hVnwL2Esnk",
   "customer_email": "juspay.jus@gmail.com",
   "customer_phone": "6369288490",
-  "payment_page_client_id": "fortum",
+  "payment_page_client_id": "your_client_id",
   "first_name":"Sim",
   "action": "paymentPage",
   "return_url": "https://juspay.in/",
-  "gateway_id": "12",
   "order_type" : "TPV_PAYMENT",
   "options.create_mandate" : "REQUIRED",
   "metadata.bank_account_details" : "[{\"bank_account_number\":\"83748239234872\",\"bank_ifsc\":\"AABC0876543\", \"juspay_bank_code\" : \"500007\", \"bank_beneficiary_name\" : \"Abc\"}]",
