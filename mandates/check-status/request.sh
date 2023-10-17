45c93d62d2e051bb45a1e19a1e581cf92c787175
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-10-17 19:17:48 +0530

    check-status

---
+++ mandates/check-status/request.sh
@@ -0,0 +1,4 @@
+curl --location --request POST 'http://localhost:8080/mandates/hyhTD5Ga4jJ2KXZwwnyh4A' \
+--header 'Authorization: Basic N0JBRDA4OUExRjYwNEYwREEyNDlCNjY3QTVCMUQzM0Q6' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'command=check_status'
