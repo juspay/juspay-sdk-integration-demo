db7c6ff3f6183868f03a13904dd413260ee74850
Arbinda Kumar Prasad <38696263+ArbindKrPrasad@users.noreply.github.com>
2023-10-25 14:08:31 +0530

    Update CheckoutViewController.swift

--- payment-page-ios-swift-integration-demo/juspay-sdk-integration-swift/CheckoutViewController.swift
+++ payment-page-ios-swift-integration-demo/juspay-sdk-integration-swift/CheckoutViewController.swift
@@ -58,59 +58,10 @@ class CheckoutViewController: UIViewController {
     
     // This create order call should be made on the merchants server
     func createOrder(completion: @escaping ([String: Any]?) -> Void) {
-        let semaphore = DispatchSemaphore(value: 0)
-        let orderId = String(Int.random(in: 10000000...99999999))
+        // This function return the sdk_payload
+        // To get the sdk_payload you need to hit your backend API which will again hit the Create Order API 
+        // and the sdk_response from the Create Order API need to be passed here
 
-        let parameters = """
-        {
-            "order_id": "\(orderId)",
-            "amount": "\(totalpayable)",
-            "customer_id": "9876543201",
-            "customer_email": "test@mail.com",
-            "customer_phone": "9876543201",
-            "payment_page_client_id": "<CLIENT_ID>",
-            "action": "paymentPage",
-            "return_url": "https://shop.merchant.com",
-            "description": "OrderDescription",
-            "first_name": "John",
-            "last_name": "wick"
-        }
-        """ 
-        
-        let postData = parameters.data(using: .utf8)
-
-        var request = URLRequest(url: URL(string: "https://api.juspay.in/session")!, timeoutInterval: Double.infinity)
-        request.addValue("Basic \(Data("<YOUR_API_KEY>".utf8).base64EncodedString())", forHTTPHeaderField: "Authorization")
-        request.addValue("<MERCHANT_ID>", forHTTPHeaderField: "x-merchantid")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-        request.httpMethod = "POST"
-        request.httpBody = postData
-
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data else {
-                semaphore.signal()
-                completion(nil)
-                return
-            }
-
-            do {
-                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-                    let sdkPayload = json["sdk_payload"] as? [String: Any] {
-                    completion(sdkPayload)
-                } else {
-                    completion(nil)
-                }
-            } catch {
-                print("Error: Failed to parse JSON - \(error)")
-                completion(nil)
-            }
-
-            semaphore.signal()
-        }
-
-        task.resume()
-        semaphore.wait()
     }
     
 
