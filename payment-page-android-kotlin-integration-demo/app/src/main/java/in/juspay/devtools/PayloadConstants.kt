cfb59ab377a8b993c06a6fcba1c1933ef460abff
Arbinda Kumar Prasad <arbinda.prasad@juspay.in>
2023-10-31 21:03:13 +0530

    frontend android java

--- payment-page-android-kotlin-integration-demo/app/src/main/java/in/juspay/devtools/PayloadConstants.kt
+++
@@ -1,7 +0,0 @@
-package `in`.juspay.devtools
-
-object PayloadConstants {
-    const val MERCHANT_ID = "" // Your merchant ID
-    const val CLIENT_ID = "hdfcmaster" // Your client ID
-    const val ENVIRONMENT = "sandbox" // Possible values: sandbox/production
-}
