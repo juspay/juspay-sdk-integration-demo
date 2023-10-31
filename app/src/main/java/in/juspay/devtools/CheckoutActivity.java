19b26dfdf74ad4b91c13dc8468ed8016f8ae9d63
Arbinda Kumar Prasad <38696263+ArbindKrPrasad@users.noreply.github.com>
2023-10-31 21:25:34 +0530

    Update CheckoutActivity.java

--- app/src/main/java/in/juspay/devtools/CheckoutActivity.java
+++ app/src/main/java/in/juspay/devtools/CheckoutActivity.java
@@ -191,48 +191,4 @@ public class CheckoutActivity extends AppCompatActivity {
         taxTv.setText("₹ "+taxString);
         totalPayableTv.setText("₹ "+amountString);
     }
-
-
-
-    /*
-    Optional Block
-    These functions are only supposed to be implemented in case if you are overriding
-    onActivityResult or onRequestPermissionsResult Life cycle hooks
-
-
-    - onActivityResult
-    - Handling onActivityResult hook and passing data to HyperServices Instance, to handle App Switch
-    @Override
-    public void onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        // block:start:onActivityResult
-
-        // If super.onActivityResult is available use following:
-        // super.onActivityResult(requestCode, resultCode, data);
-
-        // In case super.onActivityResult is NOT available please use following:
-        // if (data != null) {
-        //    hyperServices.onActivityResult(requestCode, resultCode, data);
-        // }
-
-        // block:end:onActivityResult
-
-        // Rest of your code.
-    }
-
-
-    - onRequestPermissionsResult
-    - Handling onRequestPermissionsResult hook and passing data to HyperServices Instance, to OTP reading permissions
-    @Override
-    public void onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        // block:start:onRequestPermissionsResult
-
-        //  If super.onRequestPermissionsResult is available use following:
-        // super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        // In case super.onActivityResult is NOT available please use following:
-        // hyperServices.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        // block:end:onRequestPermissionsResult
-    }
-     */
 }
