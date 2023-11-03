efb19fe81174d6232462d49d8c1c3434cc3db8dc
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-11-03 14:03:25 +0530

    udpates

--- 3rd-party/amazon-s2s/android/dependencies.java
+++ 3rd-party/amazon-s2s/android/dependencies.java
@@ -1,10 +1,3 @@
 dependencies {
- implementation 'in.juspay:amazonpay-silentpay-sdk:1.2.0'
- implementation 'in.juspay:amazonpay-hardened-silentpay-sdk:1.3.0'  
-  
- implementation 'com.jakewharton.timber:timber:4.7.1'
- implementation 'androidx.lifecycle:lifecycle-extensions:2.2.0'
- implementation 'androidx.work:work-runtime:2.7.1'
-  
- implementation "androidx.browser:browser:1.3.0"
+ implementation 'com.amazon.android.amazonpay:apay-hardened-android-sdk-sso:1.1.8'
 }
