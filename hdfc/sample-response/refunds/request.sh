06cccb5592a2308bad2888ec2900adf43feb0e5a
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-11-09 16:30:06 +0530

    hdfc-changes

---
+++ hdfc/sample-response/refunds/request.sh
@@ -0,0 +1,5 @@
+curl --location --request GET 'https://api.juspay.in/orders/JP1636474794' \
+--header 'version: 2023-06-30' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-merchantid: merchant_id'\
+--header 'Authorization: Basic OTc5Mzcx*****czRTlGOg==' 
