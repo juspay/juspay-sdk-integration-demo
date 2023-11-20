0a83a3d7ca8de11995af8450b5868896d69fe9b1
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-11-20 22:20:27 +0530

    updates

---
+++ amazon-paylater/sdk/order-status.sh
@@ -0,0 +1,8 @@
+curl --location --request GET 'https://api.juspay.in/orders/MOTOINR1695191749
+ \
+--header 'version: 2021-10-25' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'Authorization: Basic OTc5Mzcx*****czRTlGOg==' 
+
+Replace MOTOINR1695191749 with your order_id.
+And OTc5Mzcx*****czRTlGOg== with your API_KEY
