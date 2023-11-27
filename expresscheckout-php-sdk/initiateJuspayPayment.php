737626d88f4fc3e625e22d84b38b35182a2805d7
ramprakash.v <ramprakash.v@juspay.in>
2023-11-27 11:40:40 +0530

    updated checkout screen

--- expresscheckout-php-sdk/initiateJuspayPayment.php
+++ expresscheckout-php-sdk/initiateJuspayPayment.php
@@ -11,37 +11,31 @@ $config = ServerEnv::$config;
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, true);
 header('Content-Type: application/json');
-if(isset($input['amount'])) {
-    $orderId = $input['order_id'] ? $input['order_id'] : uniqid();
-    $amount = $input['amount'];
-    try {
-        $params = array();
-        $params['amount'] = $input["amount"];
-        $params['currency'] = "INR";
-        $params['order_id'] = $orderId;
-        $params["merchant_id"] = $config["MERCHANT_ID"]; # Add merchant id
-        $params['customer_id'] = "testing-customer-one";
-        $params['payment_page_client_id'] = $config["PAYMENT_PAGE_CLIENT_ID"];
-        $params['action'] = "paymentPage";
-        $params['return_url'] = "http://localhost:5000/handleJuspayResponse";
-        $requestOption = new RequestOptions();
-        $requestOption->withCustomerId("testing-customer-one")->withMerchantId($config["MERCHANT_ID"]); # Add merchant id
-        $session = OrderSession::create($params, $requestOption);
-        if ($session->status == "NEW") {
-            $response = array("orderId" => $session->orderId, "id" => $session->id, "status" => $session->status, "paymentLinks" =>  $session->paymentLinks, "sdkPayload" => $session->sdkPayload );
-        } else {
-            http_response_code(500);
-            $response = array("message" => "session status: " . $session->status);
-        }
-    } catch ( JuspayException $e ) {
-        http_response_code($e->getHttpResponseCode());
-        $response = array("message" => $e->getErrorMessage());
-        error_log($e->getErrorMessage());
+$orderId = uniqid();
+$amount = mt_rand(1,100);
+try {
+    $params = array();
+    $params['amount'] = $amount;
+    $params['currency'] = "INR";
+    $params['order_id'] = $orderId;
+    $params["merchant_id"] = $config["MERCHANT_ID"]; # Add merchant id
+    $params['customer_id'] = "testing-customer-one";
+    $params['payment_page_client_id'] = $config["PAYMENT_PAGE_CLIENT_ID"];
+    $params['action'] = "paymentPage";
+    $params['return_url'] = "http://localhost:5000/handleJuspayResponse";
+    $requestOption = new RequestOptions();
+    $requestOption->withCustomerId("testing-customer-one")->withMerchantId($config["MERCHANT_ID"]); # Add merchant id
+    $session = OrderSession::create($params, $requestOption);
+    if ($session->status == "NEW") {
+        $response = array("orderId" => $session->orderId, "id" => $session->id, "status" => $session->status, "paymentLinks" =>  $session->paymentLinks, "sdkPayload" => $session->sdkPayload );
+    } else {
+        http_response_code(500);
+        $response = array("message" => "session status: " . $session->status);
     }
-} else {
-    http_response_code(400);
-    $response = array('message' => 'Invalid data received');
-    error_log("Invalid data received");
+} catch ( JuspayException $e ) {
+    http_response_code($e->getHttpResponseCode());
+    $response = array("message" => $e->getErrorMessage());
+    error_log($e->getErrorMessage());
 }
 echo json_encode($response);
 ?>
