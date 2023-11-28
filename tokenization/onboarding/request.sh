523e0a37c180843bd7180cf2ecb6acf4eea03e0c
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-11-28 19:41:38 +0530

    fix

--- tokenization/onboarding/request.sh
+++ tokenization/onboarding/request.sh
@@ -1,7 +1,7 @@
 curl --location --request POST 'https://api.juspay.in/tokenization/onboarding' \
 --header 'Authorization: Basic base_64_encoded_api_key==' \
 --header 'version: 2023-06-30' \
---header 'x-merchantid: <merchant id> \
+--header 'x-merchantid: <merchant id>' \
 --header 'Content-Type: application/json' \
 --data-raw '{
   "associateId": "rohit_juspay",
