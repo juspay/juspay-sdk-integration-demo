a7eee6a9e228a1129464ca8c2c9f7a4b96935d68
Udit Khandelwal <udit.khandelwal@juspay.in>
2023-11-28 17:50:55 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference>card/card-info/request-mandate.sh @ api-reference/docs/card-info by Udit Khandelwal

--- card/card-info/request-mandate.sh
+++ card/card-info/request-mandate.sh
@@ -1,1 +1,2 @@
-curl --location --request GET 'https://api.juspay.in/cardbins/424242?merchant_id=merchantId&options.check_mandate_support=true'
+curl -X GET https://api.juspay.in/cardbins/405028700?merchant_id=merchantId&options.check_mandate_support=true' \
+-u your_api_key: 
