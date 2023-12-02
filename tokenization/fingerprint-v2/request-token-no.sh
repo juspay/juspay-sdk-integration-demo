0941331dcfdd448664d6f813ac0c26e14345f17d
Ankit Jain <ankit.jain@ankit.jain-MacBookAir>
2023-12-02 19:06:03 +0530

    update

--- tokenization/fingerprint-v2/request-token-no.sh
+++ tokenization/fingerprint-v2/request-token-no.sh
@@ -2,7 +2,6 @@ curl --location 'https://sandbox.juspay.in/v2/card/fingerprint' \
 --header 'Content-Type: application/json' \
 --header 'Authorization: Basic <MASKED API KEY>' \
 --data '{
-    "request_id": "Unique Request ID",
     "card_number": "Token Number",
-    "istoken": "true"
+    "is_token": true
 }'
