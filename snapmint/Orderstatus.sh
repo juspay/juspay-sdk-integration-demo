744c7cdd33ad479fe555355534c871e8d20a3d9b
srikanthjuspay <152646296+srikanthjuspay@users.noreply.github.com>
2023-12-03 21:06:36 +0530

    Create Orderstatus.sh

---
+++ snapmint/Orderstatus.sh
@@ -0,0 +1,5 @@
+curl --location --request GET 'https://api.juspay.in/orders/<orderid>' \
+--header 'version: 2023-06-30' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-merchantid: merchant_id'\
+--header 'Authorization: Basic O*************************==' 
