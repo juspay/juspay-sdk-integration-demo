f0b74165ff395c146fa737d6b821bcae25c75b00
kadrolkar <145998039+kadrolkar@users.noreply.github.com>
2023-12-26 19:08:38 +0530

    Update upi.sh

--- payout-api/order-create/upi.sh
+++ payout-api/order-create/upi.sh
@@ -1,29 +1,45 @@
-curl --location --request POST 'https://api.juspay.in/payout/merchant/v1/orders' \
--u your_api_key: \
+curl --location 'https://sandbox.juspay.in/payout/merchant/v1/orders' \
 --header 'Content-Type: application/json' \
---header 'x-merchantid: <merchant-id>' \
+--header 'x-merchantid: <merchant id>' \
+--header 'Authorization: Basic Basic (b64 encoded API key)' \
 --data-raw '{
-    "orderId": "e6J4OxzzbpJ1G2Lu",
+    "orderId": "1703597784",
     "fulfillments": [
         {
-            "preferredMethodList": [
-                "CASHFREE_IMPS"
-            ],
+            "preferredMethodList": ["DUMMY_UPI"],
             "amount": 1,
             "beneficiaryDetails": {
                 "details": {
-                    "name": "Taral Shah",
-                    "vpa" : "success@upi"
+                    "name": "Aditya Kadrolkar", 
+                    "vpa": "test@upi"
                 },
                 "type": "UPI_ID"
+            },
+               "udf1" : "String1"
+            ,  "udf2" : "String2"
+            ,  "udf3" : "String3"
+            ,  "udf4" : "String4"
+            ,  "udf5" : "String5"
+            ,
+            "additionalInfo": {
+                "remark":"Payout Transaction",
+                "useThisAsTR":"ef9eb194e664a319549e644c08be2b",
+                "scheduleTime":"2023-12-23T16:51:51Z",
+                "attemptThreshold": 25,
+                "webhookDetails":{
+                    "url" : "https://www.test.com/callback/dynamicwebhook/example"
+                    , "username" : "test"
+                    , "password" : "password"
+                    , "customHeader" :  "123456789"
+                }
             }
         }
     ],
     "amount": 1,
-    "customerId": "ce1d3404-dc3d-4886-a46e-43a48ddd0324",
+    "customerId": "cth_59Yibs1JauYP6WJP",
     "customerPhone": "9999999999",
-    "customerEmail": "taral@gmail.com",
-    "type": "FULFILL_ONLY".
+    "customerEmail": "test@gmail.com",
+    "type": "FULFILL_ONLY",
     "udf1":"udf1",
     "udf2":"udf2",
     "udf3":"udf3",
