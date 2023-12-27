afeb7ff5387bcc764435822c153840ee01401024
kadrolkar <145998039+kadrolkar@users.noreply.github.com>
2023-12-27 13:37:44 +0530

    Update bene.sh

--- payout-api/order-create/bene.sh
+++ payout-api/order-create/bene.sh
@@ -1,25 +1,48 @@
-curl --location --request POST 'https://api.juspay.in/payout/merchant/v1/orders/' \
---header 'Authorization: ***************************' \
+curl --location 'https://sandbox.juspay.in/payout/merchant/v1/orders' \
 --header 'Content-Type: application/json' \
---header 'x-merchantid: <merchant-id>' \
+--header 'x-merchantid: <merchant id>' \
+--header 'Authorization: Basic (b64 encoded API key)' \
 --data-raw '{
-    "orderId":"65YmmR7tWPxssB55",
+    "orderId": "1703659780",
     "fulfillments": [
         {
-            "preferredMethodList": ["CFGEN_IMPS"],
-            "amount": 11,
+            "preferredMethodList": ["DUMMY_IMPS"],
+            "amount": 1,
             "beneficiaryDetails": {
                 "details": {
-                    "name": "Arun Kumar",
-                    "id": "pa_dry_run00"
+                    "name": "Aditya Kadrolkar",
+                    "id": "33917851140"
                 },
                 "type": "BENE_ID"
+            },
+               "udf1" : "String1"
+            ,  "udf2" : "String2"
+            ,  "udf3" : "String3"
+            ,  "udf4" : "String4"
+            ,  "udf5" : "String5"
+            ,
+            "additionalInfo": {
+                "remark":"Payout Transaction",
+                "useThisAsTR":"ef9eb194e664a319549e644c08be2b",
+                "scheduleTime":"2023-12-23T16:51:51Z",
+                "attemptThreshold": 1440,
+                "webhookDetails":{
+                    "url" : "https://www.test.com/callback/dynamicwebhook/example"
+                    , "username" : "test"
+                    , "password" : "password"
+                    , "customHeader" :  "123456789"
+                }
             }
         }
     ],
-    "amount": 11,
-    "customerId": "arun1",
-    "customerPhone": "9999099999",
-    "customerEmail": "shubham@gmail.com",
-    "type": "FULFILL_ONLY"
+    "amount": 1,
+    "customerId": "cth_59Yibs1JauYP6WJP",
+    "customerPhone": "9999999999",
+    "customerEmail": "test@gmail.com",
+    "type": "FULFILL_ONLY",
+    "udf1":"udf1",
+    "udf2":"udf2",
+    "udf3":"udf3",
+    "udf4":"udf4",
+    "udf5":"udf5"
 }'
