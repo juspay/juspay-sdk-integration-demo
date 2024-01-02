9b06137cadc3becd194b5676d5a124a49ae29e2d
Bhagat Muskan <bhagat.muskan@juspay.in>
2024-01-02 13:36:25 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference>mandates/execution/request-session.sh @ hyper-checkout/web/mandate-execution-api by Bhagat Muskan

--- mandates/execution/request-session.sh
+++ mandates/execution/request-session.sh
@@ -1,10 +1,10 @@
 curl --location 'https://api.juspay.in/txns' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: Basic <base64 of key:>' \
---data-urlencode 'order.order_id=26234761248249834753485721' \
+--data-urlencode 'order.order_id=unique_order_id' \
 --data-urlencode 'order.amount=1.00' \
---data-urlencode 'order.customer_id=cst_lz7zmpemoo5okgav' \
+--data-urlencode 'order.customer_id=customer_id' \
 --data-urlencode 'merchant_id=merchantid' \
---data-urlencode 'mandate_id=4rKxSj3bNXs7RQcdtajAkb' \
+--data-urlencode 'mandate_id=mandate_id' \
 --data-urlencode 'mandate.execution_date=1622369936' \
 --data-urlencode 'format=json'
