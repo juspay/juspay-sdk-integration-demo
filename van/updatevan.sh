12356e15e4294f576f2e7e49cd69d7db39d6a1a2
kadrolkar <145998039+kadrolkar@users.noreply.github.com>
2024-01-03 20:49:45 +0530

    Update updatevan.sh

--- van/updatevan.sh
+++ van/updatevan.sh
@@ -1,4 +1,4 @@
-curl --location --request PUT 'https://sandbox.juspay.in/customers/:customer_id/virtual-accounts/test1' \
+curl --location --request PUT 'https://sandbox.juspay.in/customers/:customer_id/virtual-accounts/:virtual_account_reference' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: Basic QTA0QT*******ODg1Og==' \
 --header 'x-merchantid: merchant_id' \
