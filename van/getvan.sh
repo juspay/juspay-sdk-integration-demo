f779120fbc8e7443923d1bd6ef0cdf2e71c6f64c
kadrolkar <145998039+kadrolkar@users.noreply.github.com>
2024-01-03 20:49:32 +0530

    Update getvan.sh

--- van/getvan.sh
+++ van/getvan.sh
@@ -1,4 +1,4 @@
-curl --location 'https://sandbox.juspay.in/customers/:customer_id/virtual-accounts/test1' \
+curl --location 'https://sandbox.juspay.in/customers/:customer_id/virtual-accounts/:virtual_account_reference' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: Basic RjMx*******Q0I0NjMyOg=='\
 --header 'x-merchantid: merchant_id'
