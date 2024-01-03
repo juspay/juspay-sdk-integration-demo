390ec2dd873fdfd1b451103c1dc4fda298aa6e2d
kadrolkar <145998039+kadrolkar@users.noreply.github.com>
2024-01-03 20:51:14 +0530

    Create listvan.sh

---
+++ van/listvan.sh
@@ -0,0 +1,4 @@
+curl --location 'https://sandbox.juspay.in/customers/customers/:customer_id/virtual-accounts' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-merchantid: merchant_id'\
+--header 'Authorization: Basic RjMxREI*********5Q0I0NjMyOg=='
