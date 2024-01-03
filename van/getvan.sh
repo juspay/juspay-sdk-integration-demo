71aed9f4abc5c690f307d61f6c829a69cd9482db
kadrolkar <145998039+kadrolkar@users.noreply.github.com>
2024-01-03 20:46:17 +0530

    Create getvan.sh

---
+++ van/getvan.sh
@@ -0,0 +1,4 @@
+curl --location 'https://sandbox.juspay.in/customers/:customer_id/virtual-accounts/test1' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'Authorization: Basic RjMx*******Q0I0NjMyOg=='\
+--header 'x-merchantid: merchant_id'
