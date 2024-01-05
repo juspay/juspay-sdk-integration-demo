31b049ed3edde07877231d84d0a4579e67e54441
Rubai Mandal <rubai.mandal@juspay.in>
2024-01-05 19:23:53 +0530

    updates

--- add-ons/payment-links/new-request.sh
+++ add-ons/payment-links/new-request.sh
@@ -2,37 +2,29 @@ curl --location 'https://sandbox.juspay.in/session' \
 --header 'origin: https://sandbox.juspay.in' \
 --header 'Content-Type: application/json' \
 --header 'Authorization: Basic *********************API_KEY' \
---data-raw '{"currency": "INR",
+--data-raw '{
    "mobile_country_code": "+91",
-   "mandate.revokable_by_customer": true,
-   "mandate_frequency": "ASPRESENTED",
-   "options.create_mandate": "REQUIRED",
-   "mandate.block_funds": false,
-   "mandate_start_date": "2023-12-28T12:49:36",
    "payment_page_client_id": "picasso",
-   "upiCheckBox": [
-       "UPI"
-   ],
-   "netbankingCheckBox": null,
-   "cardsCheckBox": [
-       "Cards"
-   ],
-   "walletCheckBox": [
-       "Wallets"
-   ],
    "amount": 100,
+   "currency": "INR",
+   "action": "paymentPage", 
    "customer_email": "asdf@gmail.com",
-   "customer_phone": "8987654567",
-   "shouldSendSMS": true,
-   "shouldSendWhatsapp": true,
-   "shouldSendMail": true,
+   "customer_phone": "8888899999",
+   "first_name": "John",
+   "last_name" : "Doe",
    "description": "This is Payment Description.",
-   "mandate_max_amount": "1000",
-   "mandate.end_date": "1733011200",
    "customer_id": "customer_id",
-   "gateway_id": "12",
-   "order_id": "order_a_b_id_dummy_1",
+   "order_id": "order_dummy_1",
    "return_url": "https://juspay.in/",
+   "mandate.revokable_by_customer": true,
+   "mandate.block_funds": false,
+   "mandate_max_amount": "1000",
+   "mandate.start_date": "1703768016",
+   "mandate.end_date": "1733011200",
+   "mandate.frequency": "ASPRESENTED",
+   "options.create_mandate": "REQUIRED",
+   "send_mail": true,
+   "send_sms": false,
    "udf1": "udf1-dummy",
    "udf2": "udf2-dummy",
    "udf3": "udf3-dummy",
@@ -41,11 +33,8 @@ curl --location 'https://sandbox.juspay.in/session' \
    "udf5": "udf5-dummy",
    "udf7": "udf7-dummy",
    "udf8": "udf8-dummy",
-   "udf10": "udf10-dummy",
    "udf9": "udf9-dummy",
-   "mandate.frequency": "ASPRESENTED",
-   "options.create_mandate": "REQUIRED",
-   "mandate.start_date": "1703768016.296",
+   "udf10": "udf10-dummy",
    "payment_filter": {
        "allowDefaultOptions": false,
        "options": [
@@ -68,8 +57,8 @@ curl --location 'https://sandbox.juspay.in/session' \
        ],
        "emiOptions": {}
    },
+   "gateway_id": "12",
    "metadata.JUSPAY:gateway_reference_id": "payu_test",
-   "metadata.expiryInMins": "3397",
-   "merchant_id": "chetan_test"
-}
+   "metadata.expiryInMins": "3397"
+  }
 '
