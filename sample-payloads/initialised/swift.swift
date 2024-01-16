a7df2064f70f46cd8d505a798826513ed65e8e85
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2024-01-16 17:27:29 +0530

    Update swift.swift

--- sample-payloads/initialised/swift.swift
+++ sample-payloads/initialised/swift.swift
@@ -1,11 +1,10 @@
 /**
 Checks if current sdk instance has been initialised.
 */
-- (Boolean)isInitialised;
 
 //Example usage
-if hyperInstance.isInitialised {
-    hyperInstance.process(processPayload);
-} else {
+if hyperInstance.isInitialised() {
+    hyperInstance.process(processPayload)
+}else {
   //Intialize hyperInstance
 }
