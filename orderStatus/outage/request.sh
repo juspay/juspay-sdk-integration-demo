dbd6b2bd4f734fc94e561908814e9191833a4389
kadrolkar <145998039+kadrolkar@users.noreply.github.com>
2024-01-22 17:10:42 +0530

    Update request.sh

--- orderStatus/outage/request.sh
+++ orderStatus/outage/request.sh
@@ -1,3 +1,3 @@
 curl --location 'https://sandbox.juspay.in/payout/merchant/v1/outage' \
---header 'Content-Type: application/json --header' \
+--header 'Content-Type: application/json' \
 --header 'Authorization: Basic (b64 encoded API key)=='
