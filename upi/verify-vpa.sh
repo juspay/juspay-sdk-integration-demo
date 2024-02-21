fdc6e45bd51ab816379c82aaaaf6f52388800af5
Debdeepa Karmakar <debdeepa.karmakar@juspay.in>
2024-02-21 13:46:21 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference-hdfc>upi/verify-vpa.sh @ smartgateway-tranportal-integration/docs/verify-vpa by Debdeepa Karmakar

--- upi/verify-vpa.sh
+++ upi/verify-vpa.sh
@@ -1,5 +1,5 @@
 curl --location --request POST 'https://smartgateway.hdfcbank.com/v2/upi/verify-vpa' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
---header 'Authorization: Basic RDR****DQkVC****BQjc0QThCOg==' \
+--header 'Authorization: Basic QTA0QT*******ODg1Og==' \
 --data-urlencode 'vpa=9999999999@upi' \
 --data-urlencode 'merchant_id=merchantId'
