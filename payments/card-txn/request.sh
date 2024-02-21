e5d7a09e0849fe49e180dd55a72c921d3cbad747
Debdeepa Karmakar <debdeepa.karmakar@juspay.in>
2024-02-21 13:24:58 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference-hdfc>payments/card-txn/request.sh @ smartgateway-tranportal-integration/docs/credit--debit-card-transaction by Debdeepa Karmakar

--- payments/card-txn/request.sh
+++ payments/card-txn/request.sh
@@ -1,5 +1,8 @@
 // A regular card transaction
 curl -X POST https://smartgateway.hdfcbank.com/txns \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-merchantid: merchant_id'\
+--header 'Authorization: Basic QTA0QT*******ODg1Og==' \
 -d "order_id=:order_id" \
 -d "merchant_id=:merchant_id" \
 -d "payment_method_type=CARD" \
