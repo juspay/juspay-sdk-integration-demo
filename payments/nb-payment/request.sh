222fca29c9638f52a091cd86e9abf506a0d1a9bf
Debdeepa Karmakar <debdeepa.karmakar@juspay.in>
2024-02-21 13:30:06 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference-hdfc>payments/nb-payment/request.sh @ smartgateway-tranportal-integration/docs/netbanking-payment by Debdeepa Karmakar

--- payments/nb-payment/request.sh
+++ payments/nb-payment/request.sh
@@ -1,4 +1,5 @@
 curl -X POST https://smartgateway.hdfcbank.com/txns \
+--header 'Authorization: Basic QTA0QT*******ODg1Og==' \
 -d "order_id=:order_id" \
 -d "merchant_id=:merchant_id" \
 -d "payment_method_type=NB" \
