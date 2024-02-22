af2e516da50517e47181093882ac86f44ed57b1b
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2024-02-22 13:18:48 +0530

    Create request-curl.sh

---
+++ outages/request-curl.sh
@@ -0,0 +1,2 @@
+curl -X GET https://api.juspay.in/merchants/guest/paymentmethods?options.add_outage=true
+curl -X GET https://api.juspay.in/merchants/guest/paymentmethods?options.add_outage=true&options.add_upi_outages=true
