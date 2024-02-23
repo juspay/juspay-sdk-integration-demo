fdf2fb49e9c6985ae32eab4588ec8371033eb9bc
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2024-02-23 18:37:12 +0530

    Update request-encrypted.sh

--- code-samples/sample-response/refunds/request-encrypted.sh
+++ code-samples/sample-response/refunds/request-encrypted.sh
@@ -3,5 +3,6 @@ curl --location --request POST 'https://smartgatewayuat.hdfcbank.com/orders/1418
 -H "version:2023-06-30" \
 -H 'Content-Type: application/x-www-form-urlencoded'\
 -H 'x-merchantid: merchant_id'\
+-H 'x-customerid: customer_id'\
 -d "unique_request_id=xyz123" \
 -d "amount=100.00"
