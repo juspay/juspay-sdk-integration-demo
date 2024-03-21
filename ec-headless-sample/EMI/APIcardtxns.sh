988e99fa88831a416516828dfc9ea6e533992738
Priyanga7695 <99274601+Priyanga7695@users.noreply.github.com>
2024-03-21 16:20:40 +0530

    Create APIcardtxns.sh

---
+++ ec-headless-sample/EMI/APIcardtxns.sh
@@ -0,0 +1,36 @@
+// A regular card transaction
+curl -X POST https://api.juspay.in/txns \
+-d "order_id=:order_id" \
+-d "merchant_id=:merchant_id" \
+-d "payment_method_type=CARD" \
+-d "payment_method=VISA" \
+-d "card_number=4242424242424242" \
+-d "card_exp_month=10" \
+-d "card_exp_year=20" \
+-d "name_on_card=Name" \
+-d "card_security_code=111" \
+-d "save_to_locker=true" \
+-d "is_emi=true" \
+-d "emi_bank=HDFC" \
+-d "emi_tenure=3" \
+-d "emi_type=STANDARD_EMI" \
+-d "supported_gateway_id=[27,74]" \
+-d "tokenize=true" \
+-d "redirect_after_payment=true" \
+-d "format=json"
+
+
+// A Stored card transaction
+curl -X POST https://api.juspay.in/txns \
+-d "order_id=:order_id" \
+-d "merchant_id=:merchant_id" \
+-d "payment_method_type=CARD" \
+-d "card_token=:card_token" \
+-d "is_emi=true" \
+-d "emi_bank=HDFC" \
+-d "emi_tenure=3" \
+-d "emi_type=STANDARD_EMI" \
+-d "supported_gateway_id=[27,74]" \
+-d "card_security_code=111" \ #optional field for CVV less supported transactions
+-d "redirect_after_payment=true" \
+-d "format=json"
