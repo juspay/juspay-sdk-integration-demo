f460846a34d006d4ba0cd320cdb9d28b5b917efe
Debdeepa Karmakar <debdeepa.karmakar@juspay.in>
2024-03-29 11:20:07 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference-hdfc>add-ons/payment-links/new-request.sh @ smartgateway-payment-links/docs/session-api--payment-links by Debdeepa Karmakar

--- add-ons/payment-links/new-request.sh
+++ add-ons/payment-links/new-request.sh
@@ -42,6 +42,7 @@ curl --location --request POST 'https://smartgatewayuat.hdfcbank.com/session' \
 "mandate.end_date":"1763971322",
 "mandate.frequency" : "MONTHLY",
 "metadata.expiryInMins" : "15",
+"metadata.JUSPAY:gatewayReferenceId": "payu_test",
 "source_object" : "PAYMENT_LINK",
 "udf1": "udf1-dummy",
 "udf2": "udf2-dummy",
