fb6fbe4625160d9c97e533c55b51b6404b995cba
kadrolkar <145998039+kadrolkar@users.noreply.github.com>
2024-04-03 13:11:16 +0530

    Create resolve.sh

---
+++ payout-api/account-validation/resolve.sh
@@ -0,0 +1,20 @@
+curl --location 'https://api.juspay.in/payout/merchant/v1/benedetails' \
+--header 'Content-Type: application/json' \
+--header 'x-merchantid: <merchant id>' \
+--header 'Authorization: Basic (b64 encoded API key)' \
+--data-raw '{
+    "beneId":"beneID11",
+    "beneDetails" : {
+                "details": {
+                    "name": "Aditya Kadrolkar",
+                    "mobile": "8088375524"
+                    },
+                "type": "UPI_ID"
+            },
+    "preferredGateway" : "YESBIZ",
+    "command" : "resolve",
+    "customerId" : "14212551",
+    "email": "test@gmail.com",
+    "phone": "8088375524"
+
+}'
