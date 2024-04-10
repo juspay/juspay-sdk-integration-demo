16ea3966cbe38fdd0eb8f1e4c9c51176a41caa76
VivekJus <162992597+VivekJus@users.noreply.github.com>
2024-04-10 14:03:37 +0530

    Create uat_req.sh

---
+++ LOYLTYREWARDZ/ec-sdk/txn/submitOtp/uat_req.sh
@@ -0,0 +1,5 @@
+curl --location 'http://sandbox.juspay.in/v2/txns/<txn_uuid>/authenticate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'Authorization: Basic xxxxzxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' \
+--data-urlencode 'answer.otp=071978' \
+--data-urlencode 'challenge_id=1000012422'
