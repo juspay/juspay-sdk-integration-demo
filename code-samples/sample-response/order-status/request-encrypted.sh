cfe30603dfa9ddca14922e66314fa40c3120ddf0
Prathamesh Ramesh Rane <prathamesh.rane@juspay.in>
2024-04-17 12:42:17 +0530

    change: github>juspay>juspay-sdk-integration-demo>hdfc-sample-codes>code-samples/sample-response/order-status/request-encrypted.sh @ smartgateway-api-ref-basicauth/docs/order-status-api by Prathamesh Ramesh Rane

--- code-samples/sample-response/order-status/request-encrypted.sh
+++ code-samples/sample-response/order-status/request-encrypted.sh
@@ -1,4 +1,4 @@
-curl --location --request POST 'https://smartgatewayuat.hdfcbank.com/orders/testorder123' \
+curl --location --request POST 'https://smartgatewayuat.hdfcbank.com/orders/JP1636474794' \
 --header 'version: 2023-06-30' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'x-merchantid: merchant_id'\
