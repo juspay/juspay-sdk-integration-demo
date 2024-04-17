8a805562e84b69e61b289c331ed4bee8adb2c250
Prathamesh Ramesh Rane <prathamesh.rane@juspay.in>
2024-04-17 12:41:57 +0530

    change: github>juspay>juspay-sdk-integration-demo>hdfc-sample-codes>code-samples/sample-response/order-status/request-encrypted.sh @ smartgateway-api-ref-basicauth/docs/order-status-api by Prathamesh Ramesh Rane

--- code-samples/sample-response/order-status/request-encrypted.sh
+++ code-samples/sample-response/order-status/request-encrypted.sh
@@ -1,4 +1,4 @@
-curl --location --request POST 'https://smartgatewayuat.hdfcbank.com/v4/order-status' \
+curl --location --request POST 'https://smartgatewayuat.hdfcbank.com/orders/testorder123' \
 --header 'version: 2023-06-30' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'x-merchantid: merchant_id'\
