164beec5422c7e6f38b00f694e8c713fcbdea64c
aklankjuspay <168088064+aklankjuspay@users.noreply.github.com>
2024-04-26 15:25:16 +0530

    Create stop_madate_execution.sh

---
+++ api-reference/mandates/mandate_status/stop_madate_execution.sh
@@ -0,0 +1,5 @@
+curl --location 'http://localhost:8080/mandates/vcPEf8ZJ3Tf7i8qFhCPEDs' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'Authorization: Basic QjU3QTJCMTU5RTAxNEJGQjkyNzdEQjg1MTg4N0FCMDU6' \
+--data-urlencode 'command=cancel_recurring_transaction' \
+--data-urlencode 'order_id=test_recurring_1697711432'
