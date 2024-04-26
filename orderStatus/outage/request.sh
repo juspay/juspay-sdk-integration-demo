522f096035b93b04dc79deea93385155d060ada6
Vivek Vikram <vivek.vikram@juspay.in>
2024-04-26 17:03:04 +0530

    change: github>juspay>juspay-sdk-integration-demo>juspay-payout>orderStatus/outage/request.sh @ jusbiz/docs/outage-api by Vivek Vikram

--- orderStatus/outage/request.sh
+++ orderStatus/outage/request.sh
@@ -1,3 +1,1 @@
-curl --location 'https://sandbox.juspay.in/payout/merchant/v1/outage' \
---header 'Content-Type: application/json' \
---header 'Authorization: Basic (b64 encoded API key)=='
+curl --location 'https://biz.juspay.in/ardra/health'
