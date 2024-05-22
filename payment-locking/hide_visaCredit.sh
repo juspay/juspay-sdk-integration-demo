caafe8de232d69581eebf062e9b635b1f486d991
srikanthjuspay <152646296+srikanthjuspay@users.noreply.github.com>
2024-05-22 16:53:31 +0530

    Create hide_visaCredit.sh
    hide only visa and credit cards

---
+++ payment-locking/hide_visaCredit.sh
@@ -0,0 +1,32 @@
+--data-raw '{
+    "order_id": "jp_1716373008",
+    "amount": "10.0",
+    "customer_id": "1234",
+    "customer_phone": "9110234217",
+    "payment_page_client_id": "hdfcmaster",
+    "action": "paymentPage",
+    "return_url": "https://www.google.com",
+    "description": "Complete your payment",
+    "first_name": "John",
+    "last_name": "wick",
+    "payment_filter": {
+        "allowDefaultOptions": true,
+        "options": [
+            {
+                "paymentMethodType": "CARD",
+                "enable": true,
+                "cardFilters": [
+                    {
+                        "enable": false,
+                        "cardBrands": [
+                            "VISA"
+                        ],
+                        "cardTypes": [
+                            "CREDIT"
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}'
