26b13bd63252f5bdb7621f378a82315fcef063a3
Garima Suryavanshi <garima.suryavanshi@juspay.in>
2024-06-04 13:23:24 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference>customer/get-customer/request.sh @ api-reference/docs/getcustomer by Garima Suryavanshi

--- customer/get-customer/request.sh
+++ customer/get-customer/request.sh
@@ -1,5 +1,5 @@
 curl -X GET \
-  https://api.juspay.in/customers/:customerid \
+  https://api.hyperpg.in/customers/:customerid \
   -u your_api_key:  \
   -H 'x-merchantid: merchant_id'\
   -H 'Content-Type: application/x-www-form-urlencoded'\
