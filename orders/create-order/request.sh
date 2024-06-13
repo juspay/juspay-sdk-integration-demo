7520661e26474c645db02eac05e72035257d914a
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2024-06-13 19:30:55 +0530

    CHANGE: At api-reference/docs/express-checkout/create-order-api by Maheti Mehra (#478)
    * change: github>juspay>juspay-sdk-integration-demo>api-reference>orders/create-order/request.sh @ api-reference/docs/create-order-api by Maheti Mehra
    
    * change: github>juspay>juspay-sdk-integration-demo>api-reference>orders/order-status/request.sh @ api-reference/docs/order-status-api by Maheti Mehra
    
    ---------
    
    Co-authored-by: Maheti Mehra <maheti.mehra@juspay.in>

--- orders/create-order/request.sh
+++ orders/create-order/request.sh
@@ -1,5 +1,4 @@
 curl --location --request POST 'https://api.juspay.in/orders' \
---header 'version: 2023-06-30' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'x-merchantid: merchant_id'\
 --header 'Authorization: Basic QTA0QT*******ODg1Og==' \
