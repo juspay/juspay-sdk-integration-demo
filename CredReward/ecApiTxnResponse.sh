987869e6f420eea17b849c158acb5c206e76bc16
AkashSinghania <akash.singhania@juspay.in>
2024-06-19 17:50:05 +0530

    Create ecApiTxnResponse.sh (#491)

---
+++ CredReward/ecApiTxnResponse.sh
@@ -0,0 +1,16 @@
+{
+	"order_id": "MDHBJSIA3e84",
+	"status": "CHARGED",
+	"payment": {
+    	"authentication": {
+        	"url": "https://api.juspay.in/v2/pay/start/test_merchant/mozxr6e6qL2AEeCgSBJ?cardIssuerBankName%3DICICI%20Bank%26cardType%3DCREDIT%26paymentMethod%3DVISA%26paymentMethodType%3DCARD",
+        	"method": "GET"
+    	}
+	},
+	"txn_uuid": "mozxr6e6qL2AEeCgSBJ",
+	"merchant_return_url": "https://www.meesho.com/mcheckout/orderStatus",
+	"offer_details": {
+    	"offers": []
+	},
+	"txn_id": "test-MDHBJSIA3e84-1"
+}
