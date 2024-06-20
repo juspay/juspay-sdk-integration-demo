9eab506d3635d9c23db39c9a357ac8419f01ad8d
AkashSinghania <akash.singhania@juspay.in>
2024-06-20 18:35:05 +0530

    Update ecApiTxnRequest.sh (#494)

--- CredReward/ecApiTxnRequest.sh
+++ CredReward/ecApiTxnRequest.sh
@@ -6,7 +6,7 @@ Payload
 	payment_method_type=CARD,
 	payment_channel=WEB,
 	order_id=MDHBJSIA3e84,
-metadata=%7B%22pp_mode%22%3A%22RELEASE%22%2C%22payment_channel%22%3A%22ANDROID%22%2C%22microapp%22%3A%22ec%22%2C%20cred_offer_data%20%3A%20%22credOfferData%22%3A%20%5B%0A%7B%0A%22paymentMethodType%22%3A%20%22card%22%2C%0A%22offerData%22%3A%20%5B%0A%7B%0A%22preActionNudge%22%3A%20%22Pay%20via%20any%20credit%20card%20%26%20get%20up%20to%20%E2%82%B9150%20cashback%20on%20CRED.%22%2C%0A%22postActionNudge%22%3A%20%22claim%20your%20cashback%20on%20CRED%22%0A%22paymentMethodType%22%3A%20%22CARD%22%2C%0A%22paymentMethod%22%3A%20%22CREDIT%22%2C%0A%22isEligible%22%3A%20true%0A%7D%0A%09%09%20%20%20%5D%0A%09%20%20%20%20%7D%0A%09%20%5D%0A%2C%0A%20%20%20%20cred_user_id%20%3A%20%22testcreduserID%22%7D,
+	metadata= {"pp_mode":"RELEASE","payment_channel":"ANDROID", "cred_offer_data" : [{ "payment_method_type" : "card"}]},
 	merchant_id=test_merchant,
 	format=json,
 	client_auth_token=tkn_cdba4878f984432092d2fb7be4654609,
