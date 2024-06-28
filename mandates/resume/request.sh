b124c3cf32d855a42dff1efea8ec0d4a5f9893e7
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2024-06-28 14:54:04 +0530

    CHANGE: At smartgateway-mandate-integration/docs/express-checkout/pause-mandate by Aniket Lawaniya (#503)
    * change: github>juspay>juspay-sdk-integration-demo>api-reference-hdfc>mandates/pause/request.sh @ smartgateway-mandate-integration/docs/pause-mandate by Aniket Lawaniya
    
    * change: github>juspay>juspay-sdk-integration-demo>api-reference-hdfc>mandates/resume/request.sh @ smartgateway-mandate-integration/docs/resume-mandate-api by Aniket Lawaniya
    
    ---------
    
    Co-authored-by: Aniket Lawaniya <aniket.lawaniya@juspay.in>

--- mandates/resume/request.sh
+++ mandates/resume/request.sh
@@ -1,4 +1,4 @@
-curl --location --request POST 'http://api.juspay.in/mandates/7KKT3yu3LcQjskY5F4moS5M' \
+curl --location --request POST 'http://smartgatewayuat.hdfcbank.com/mandates/7KKT3yu3LcQjskY5F4moS5M' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: Basic QjU3Qxxxxxxxxyuegj2ehkQ0MTg4N0NEMDU6' \
 --data-urlencode 'command=resume' \
