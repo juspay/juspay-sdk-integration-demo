a5b27ebd50ce5d3d113d08ba73392bfc67ae266d
Prathamesh Ramesh Rane <prathamesh.rane@juspay.in>
2024-07-01 13:19:46 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference-hdfc>upi/collect.sh @ smartgateway-tranportal-integration/docs/upi-collect by Prathamesh Ramesh Rane

--- upi/collect.sh
+++ upi/collect.sh
@@ -4,7 +4,7 @@ curl -X POST https://smartgateway.hdfcbank.com/txns \
 -d "order_id=:order_id" \
 -d "merchant_id=:merchant_id" \
 -d "payment_method_type=UPI" \
--d "payment_method=UPI" \
+-d "payment_method=UPI_COLLECT" \
 -d "txn_type=UPI_COLLECT" \
 -d "upi_vpa=:vpa" \
 -d "redirect_after_payment=true" \
