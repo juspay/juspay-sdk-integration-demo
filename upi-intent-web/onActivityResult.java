1f188834bcedf5dcab57ef0bb30bf070087ed016
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2024-07-08 13:19:43 +0530

    CHANGE: At resources/docs/common-resources/upi-intent-support-on-webview by Rubai Mandal (#518)
    * change: github>juspay>juspay-sdk-integration-demo>juspay-api-samples>upi-intent-web/dependency.java @ resources/docs/upi-intent-support-on-webview by Rubai Mandal
    
    * change: github>juspay>juspay-sdk-integration-demo>juspay-api-samples>upi-intent-web/onActivityResult.java @ resources/docs/upi-intent-support-on-webview by Rubai Mandal

--- upi-intent-web/onActivityResult.java
+++ upi-intent-web/onActivityResult.java
@@ -1,7 +1,7 @@
 @Override
 protected void onActivityResult(int requestCode, int resultCode, Intentdata) { 
     super.onActivityResult(requestCode, resultCode, data);
-    if (requestCode  HyperWebViewServices.UPI_REQUEST_CODE) {
+    if (requestCode == HyperWebViewServices.UPI_REQUEST_CODE) {
         hyperWebViewServices.onActivityResult(requestCode, resultCode,data); 
     }
 }
