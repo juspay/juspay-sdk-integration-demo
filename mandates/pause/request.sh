5904d309550028a71f42bda9fb592e25bdf0691a
Prathamesh Ramesh Rane <prathamesh.rane@juspay.in>
2024-07-09 18:48:28 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference-hdfc>mandates/pause/request.sh @ smartgateway-mandate-integration/docs/pause-mandate by Prathamesh Ramesh Rane

--- mandates/pause/request.sh
+++ mandates/pause/request.sh
@@ -1,6 +1,4 @@
 curl --location --request POST 'http://smartgatewayuat.hdfcbank.com/mandates/7KKTSsVLcQjskY5F4moS5M' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: Basic QjU3QTnwedgfqdeuiedkyNzhEQjQ0MTg4N0NEMDU6' \
---data-urlencode 'command=pause' \
---data-urlencode 'pause_end_date=1664200815' \
---data-urlencode 'pause_start_date=1664546405'
+--data-urlencode 'command=pause'
