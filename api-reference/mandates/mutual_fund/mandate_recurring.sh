0813ec1212e2964e6c1c90450fd2a3a8e89685e8
aklankjuspay <168088064+aklankjuspay@users.noreply.github.com>
2024-07-10 15:39:25 +0530

    Create mandate_recurring.sh (#523)

---
+++ api-reference/mandates/mutual_fund/mandate_recurring.sh
@@ -0,0 +1,9 @@
+curl --location --globoff '{{local}}/txns' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'Authorization: Basic RThGQjJCMDBDODA5NDU3RkE5MzU2RDIxMzI1QUYxRDU6' \
+--data-urlencode 'order.order_id=recurring960' \
+--data-urlencode 'mandate_id=vw2pUwv8YFiBi4vZQC' \
+--data-urlencode 'order.amount=1.00' \
+--data-urlencode 'merchant_id=azharamin' \
+--data-urlencode 'mandate.notification_id=NotifyMandateIcici1' \
+--data-urlencode 'order.mutual_fund_details=[{"memberId":"ABCDE","userId":"ABCDEFGHIJ","mfPartner":"BSE","folioNumber":"190983010","orderNumber":"order_zer12345ssss","amount":"800","schemeCode":"LT","amcCode":"UYTIUI","panNumber":"TYLIO7823U","investmentType":"LUMPSUM"},{"memberId":"ABCDE","userId":"ABCDEFGHIJ","mfPartner":"BSE","folioNumber":"190983010","orderNumber":"order_zer12345sssss","amount":"800.00","schemeCode":"LT","amcCode":"UYTIUI","panNumber":"TYLIO7823U","investmentType":"LUMPSUM"}]'
