99f2af22426f2213139d792cae1e661194897d86
aklankjuspay <168088064+aklankjuspay@users.noreply.github.com>
2024-07-12 16:33:18 +0530

    Create upi_collect_new_mandate.sh

---
+++ api-reference/mandates/transactions/upi_collect_new_mandate.sh
@@ -0,0 +1,11 @@
+curl --location 'https://api.juspay.in/txns' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'Authorization: Basic MzJCNDM3QTZCNUI0OEE5OUEwQTQ4ODU4RDNCQzU1Og==' \
+--data-urlencode 'order_id=test_4_aj' \
+--data-urlencode 'merchant_id=mandate_juspay' \
+--data-urlencode 'payment_method_type=UPI' \
+--data-urlencode 'payment_method=UPI_COLLECT' \
+--data-urlencode 'redirect_after_payment=true' \
+--data-urlencode 'format=json' \
+--data-urlencode 'should_create_mandate=true' \
+--data-urlencode 'mandate_type=EMANDATE'
