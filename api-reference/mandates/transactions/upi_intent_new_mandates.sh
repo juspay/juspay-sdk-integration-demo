f318670433ea48659ffcb59cb85d3670391299c3
aklankjuspay <168088064+aklankjuspay@users.noreply.github.com>
2024-07-12 16:30:16 +0530

    Create upi_intent_new_mandates.sh

---
+++ api-reference/mandates/transactions/upi_intent_new_mandates.sh
@@ -0,0 +1,12 @@
+curl --location 'https://api.juspay.in/txns' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'Authorization: Basic MzJCNDMg==' \
+--data-urlencode 'order_id=test_aj' \
+--data-urlencode 'merchant_id=mandate_juspay' \
+--data-urlencode 'payment_method_type=UPI' \
+--data-urlencode 'payment_method=UPI_PAY' \
+--data-urlencode 'redirect_after_payment=true' \
+--data-urlencode 'format=json' \
+--data-urlencode 'sdk_params=true' \
+--data-urlencode 'should_create_mandate=true' \
+--data-urlencode 'mandate_type=EMANDATE'
