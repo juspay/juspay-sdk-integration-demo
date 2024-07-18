347ab5dad383b2e2c48750cdbe00229103e27e0b
Priyanga7695 <99274601+Priyanga7695@users.noreply.github.com>
2024-07-18 12:24:32 +0530

    UPIapitxnsreq.sh (#546)

--- TPVPayloads/UPIapitxnsreq.sh
+++ TPVPayloads/UPIapitxnsreq.sh
@@ -1,7 +1,7 @@
 curl --location --request POST 'https://sandbox.juspay.in/txns' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'order_id=1591896736' \
---data-urlencode 'merchant_id=paypal' \
+--data-urlencode 'merchant_id=JuspayUAT' \
 --data-urlencode 'payment_method_type=UPI' \
 --data-urlencode 'payment_method=COLLECT' \
 --data-urlencode 'redirect_after_payment=true' \
