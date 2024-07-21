e61cf849d8c80ebe30323e4c80b163c7227e94c4
Priyanga7695 <99274601+Priyanga7695@users.noreply.github.com>
2024-07-22 00:00:30 +0530

    refundreq.sh

--- TPVPayloads/refundreq.sh
+++ TPVPayloads/refundreq.sh
@@ -2,4 +2,4 @@ curl --location --request POST 'https://api.juspay.in/orders/testorder1/refunds'
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: Basic <base64 of Merchant API Key>' \
 --data-urlencode 'unique_request_id=refundstar7695' \
---data-urlencode 'amount=1'
+--data-urlencode 'amount=10000'
