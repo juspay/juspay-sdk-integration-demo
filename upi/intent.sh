22929e4e1c261b4634176e26e1922de7353832a4
kdebdeepa <163527548+kdebdeepa@users.noreply.github.com>
2024-07-31 19:24:27 +0530

    Update intent.sh

--- upi/intent.sh
+++ upi/intent.sh
@@ -1,10 +1,10 @@
 curl -X POST https://smartgateway.hdfcbank.com/txns \
 -H 'Authorization: Basic QTA0QT*******ODg1Og==' \
 -H 'Content-Type: application/x-www-form-urlencoded' \
--d 'merchant_id=juspay' \
--d 'order_id=9727125664Q20200107180320' \
--d 'payment_method_type=UPI' \
--d 'payment_method=UPI_PAY' \
--d 'txn_type=UPI_PAY' \
--d 'redirect_after_payment=true' \
--d 'format=json&sdk_params=true'
+-d "merchant_id=juspay" \
+-d "order_id=9727125664Q20200107180320" \
+-d "payment_method_type=UPI" \
+-d "payment_method=UPI_PA" \
+-d "txn_type=UPI_PAY" \
+-d "redirect_after_payment=true" \
+-d "format=json&sdk_params=true"
