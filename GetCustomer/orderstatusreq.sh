379e7972946c837a3f5eb331fdd4f127f6a6f45d
mohanjuspay <mohan.manikantasai@juspay.in>
2024-08-20 08:03:45 +0530

    Create orderstatusreq.sh

---
+++ GetCustomer/orderstatusreq.sh
@@ -0,0 +1,4 @@
+curl --location --request GET 'https://api.hyperpg.in/orders/JP1636474794' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-merchantid: merchant_id'\
+--header 'Authorization: Basic OTc5Mzcx*****czRTlGOg=='
