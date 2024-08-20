e73de100d0ec85a32bd3ab74473d15e21b92aa24
mohanjuspay <mohan.manikantasai@juspay.in>
2024-08-20 16:43:52 +0530

    Create ordertxnQRreq.sh (#612)

---
+++ GetCustomer/ordertxnQRreq.sh
@@ -0,0 +1,16 @@
+curl --location --request POST 'https://api.hyperpg.in/orders' \
+--header 'Authorization: Basic N0JBRDA4OUExRjYwNEYwREEyNDlCNjY3QTVCMUQzM0Q6' \
+--form 'order_id="HD-1707740261"' \
+--form 'amount="1"' \
+--form 'currency="INR"' \
+--form 'customer_id="cth_w2gbGjGi8BUkLgCo"' \
+--form 'customer_email="krishna.gprasad@juspay.in"' \
+--form 'customer_phone="7093125824"' \
+--form 'description="NA"' \
+--form 'product_id="5"' \
+--form 'return_url="http://www.google.com/"' \
+--form 'options.get_client_auth_token="true"' \
+--form 'metadata.JUSPAY:gateway_reference_id="testmode"' \
+–-form 'udf10="RiderQR"' \
+–-form 'order_fullfillment_threshold_in_mins="60"' \
+--form 'options.get_upi_qr="true"'
