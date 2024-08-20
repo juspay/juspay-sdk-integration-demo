a03f30774feffecb5914bfbd5aa428cd4fcd98d0
mohanjuspay <mohan.manikantasai@juspay.in>
2024-08-20 07:59:30 +0530

    Create updateorderreq.sh

---
+++ GetCustomer/updateorderreq.sh
@@ -0,0 +1,5 @@
+curl -X POST https://api.hyperpg.in/orders/JPAYNEW035 \
+-u your_api_key: \
+-H 'Content-Type: application/x-www-form-urlencoded'\
+-H 'x-merchantid: merchant_id'\
+-d "amount=90.00"
