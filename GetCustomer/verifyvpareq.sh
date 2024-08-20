e3a9a3490c93b816a8b84d8d0360ebf412a7b4e6
mohanjuspay <mohan.manikantasai@juspay.in>
2024-08-20 08:12:46 +0530

    Create verifyvpareq.sh

---
+++ GetCustomer/verifyvpareq.sh
@@ -0,0 +1,5 @@
+curl --location --request POST 'https://api.hyperpg.in/v2/upi/verify-vpa' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'Authorization: Basic RDR****DQkVC****BQjc0QThCOg==' \
+--data-urlencode 'vpa=9999999999@upi' \
+--data-urlencode 'merchant_id=merchantId'
