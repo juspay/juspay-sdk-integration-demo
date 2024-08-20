edfda40303c8cb812846524558202f1b99f37b0c
mohanjuspay <mohan.manikantasai@juspay.in>
2024-08-20 12:17:28 +0530

    Create Createcusreq.sh (#602)

---
+++ GetCustomer/Createcusreq.sh
@@ -0,0 +1,11 @@
+curl -X POST https://api.hyperpg.in/customers\
+-u your_api_key:\
+-H 'x-merchantid: merchant_id'\
+-H 'Content-Type: application/x-www-form-urlencoded'\
+-d "object_reference_id=customer@gmail.com"\
+-d "mobile_number=9988776655"\
+-d "email_address=customer@gmail.com"\
+-d "first_name=John"\
+-d "last_name=Smith"\
+-d "mobile_country_code=91"\
+-d"options.get_client_auth_token=true"\
