888ab7c3e368efe10ce72dd3b9215c30c570d6da
mohanjuspay <mohan.manikantasai@juspay.in>
2024-08-20 08:11:05 +0530

    Create upiintentres.sh

---
+++ GetCustomer/upiintentres.sh
@@ -0,0 +1,20 @@
+{
+  "txn_uuid": "eulgxNx4mgcnKwoqe2",
+  "txn_id": "9727125664Q20200107180320",
+  "status": "PENDING_VBV",
+  "payment": {
+    "sdk_params": {
+      "tr": "202101345671229366",
+      "tid": "121313202101345671229366",
+      "merchant_vpa": "juspay@axisbank",
+      "merchant_name": "juspay",
+      "mcc": "1234",
+      "amount": "1"
+    },
+    "authentication": {
+      "url": "https://api.hyperpg.in/v2/pay/start/juspay/eulgxNxxxxcnKwoqe2",
+      "method": "GET"
+    }
+  },
+  "order_id": "9727125664Q20200107180320"
+}
