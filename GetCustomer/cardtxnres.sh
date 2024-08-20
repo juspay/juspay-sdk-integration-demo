2c0bf171bd5fb11e244da9fe561e2e437bf145b5
mohanjuspay <mohan.manikantasai@juspay.in>
2024-08-20 08:07:37 +0530

    Create cardtxnres.sh

---
+++ GetCustomer/cardtxnres.sh
@@ -0,0 +1,11 @@
+{
+  "order_id": ":order_id",
+  "txn_id": ":txn_id",
+  "status": "PENDING_VBV",
+  "payment": {
+    "authentication": {
+      "method": "GET",
+      "url": "https://api.hyperpg.in/pay/start/guest/8646a7a0xxx74fxxxx95fec00340a507"
+    }
+  }
+}
