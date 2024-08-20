ce03d108c6aeb63e1367f22cc9a55c0e4df797c5
mohanjuspay <mohan.manikantasai@juspay.in>
2024-08-20 16:44:03 +0530

    Create GetCustomordertxndeeplinkres.sh (#611)
    * Create GetCustomordertxndeeplinkres.sh
    
    * Update GetCustomordertxndeeplinkres.sh

---
+++ GetCustomordertxndeeplinkres.sh
@@ -0,0 +1,40 @@
+{
+    "status": "NEW",
+    "status_id": 10,
+    "id": "ordeh_5d4f1f5d2135453aadd08703297c0c2b",
+    "order_id": "orderIdIs67267",
+    "payment_links": {
+        "iframe": "https://api.hyperpg.in/orders/ordeh_5d4f1f5d2135453aadd08703297c0c2b/payment-page",
+        "web": "https://api.hyperpg.in/orders/ordeh_5d4f1f5d2135453aadd08703297c0c2b/payment-page",
+        "mobile": "https://api.hyperpg.in/orders/ordeh_5d4f1f5d2135453aadd08703297c0c2b/payment-page"
+    },
+    "deep_link": "upi://pay?tr=mandate_juspay-orderIdIs67267-1&mc=5816&pa=STAGEONLINE@ybl&pn=Stage&am=2.00&cu=INR",
+    "udf9": "",
+    "udf8": "",
+    "udf7": "",
+    "udf6": "",
+    "udf5": "",
+    "udf4": "",
+    "udf3": "",
+    "udf2": "",
+    "udf10": "",
+    "udf1": "",
+    "return_url": "",
+    "refunded": false,
+    "product_id": "",
+    "merchant_id": "mandate_juspay",
+    "date_created": "2023-12-15T10:54:52Z",
+    "customer_phone": "9197079735",
+    "customer_id": "cth_nfhDAjJf8HmnZ1AK8",
+    "customer_email": "ghge@gmail.com",
+    "currency": "INR",
+    "amount_refunded": 0,
+    "source_object": "",
+    "source_object_id": "",
+    "amount": 2,
+    "juspay": {
+        "client_auth_token_expiry": "2023-12-15T11:09:52Z",
+        "client_auth_token": "tkn_af91a58f472c419b97ab10fe0f259020"
+    },
+    "order_expiry": "2023-12-15T11:09:52Z"
+}
