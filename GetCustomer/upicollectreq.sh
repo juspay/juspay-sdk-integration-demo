5a30b72a37020c57ef92f18ba734337f739aa616
mohanjuspay <mohan.manikantasai@juspay.in>
2024-08-20 08:08:34 +0530

    Create upicollectreq.sh

---
+++ GetCustomer/upicollectreq.sh
@@ -0,0 +1,8 @@
+curl -X POST https://api.hyperpg.in/txns \
+-d "order_id=:order_id" \
+-d "merchant_id=:merchant_id" \
+-d "payment_method_type=UPI" \
+-d "payment_method=UPI_COLLECT" \
+-d "upi_vpa=:vpa" \
+-d "redirect_after_payment=true" \
+-d "format=json"
