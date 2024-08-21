47b2454a5708b01729cfa71514b37aea16c7091e
Bose <bose.as@juspay.in>
2024-08-21 14:29:37 +0530

    Customer

---
+++ documentation/Core_Resources/Customer/Create/request.sh
@@ -0,0 +1,6 @@
+$ curl https://api-test.lotuspay.com/v1/customers/CU004433221AA \
+    -u sk_test_XjIHowXWSI23uvjepz2X82:
+    -d email="test@lotuspay.com" \
+    -d mobile="+91-9123456789" \
+    -d name="Amit Jain" \
+    -d pan="ABCDE1234A"
