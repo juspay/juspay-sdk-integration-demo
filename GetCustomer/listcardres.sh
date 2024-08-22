f62fc44425f2ebf25491d029f8a28daf9b9c8d05
mohanjuspay <mohan.manikantasai@juspay.in>
2024-08-22 18:20:07 +0530

    Create listcardres.sh (#667)

---
+++ GetCustomer/listcardres.sh
@@ -0,0 +1,87 @@
+{
+    "customer_id": "customer_id",
+    "merchantId": "merchant_id",
+    "cards": [
+        {
+            "expired": false,
+            "card_issuer_country": "INDIA",
+            "name_on_card": "Juspay",
+            "card_reference": "token_4149513e22e2e58ead99e1ecdd541f10",
+            "card_token": "tkn_1b6fcc1015944fdfb5020a92x4fa1f91",
+            "card_issuer": "SBI",
+            "card_brand": "VISA",
+            "card_number": "XXXX-XXXXXXXX-1449",
+            "card_fingerprint": "27po8sn3gehxumesqx9oi6xmv9",
+            "extended_card_type": "CREDIT",
+            "card_type": "CREDIT",
+            "token": {
+                "last_four_digits": "5937",
+                "card_reference": "token_4149513e22e2e58ead99e1ecdd541f10",
+                "card_fingerprint": "27po8sn3gehxumesqx9oi6xmv9",
+                "par": "V0010013820031519535347554746",
+                "card_isin": "486876061",
+                "tokenization_status": "ACTIVE",
+                "expiry_year": "2025",
+                "expiry_month": "01",
+                "cvv_less_support": true,
+                "cvv_less_supported_gateways": [
+                    "CYBERSOURCE",
+                    "RAZORPAY",
+                    "PAYTM_V2",
+                    "PAYU"
+                ]
+            },
+            "provider_category": "NETWORK",
+            "card_isin": "461119",
+            "juspay_bank_code": "JP_SBI",
+            "metadata": {
+                "origin_merchant_id": "your_merchantID"
+            },
+            "tokenize_support": false,
+            "nickname": "",
+            "card_sub_type": "VISA PLATINUM",
+            "card_exp_year": "2025",
+            "provider": "VISA",
+            "card_exp_month": "01"
+        },
+        {
+            "expired": false,
+            "card_issuer_country": "INDIA",
+            "name_on_card": "Juspay",
+            "card_reference": "token_4149513e23e2e5gead99e17cdd541f10",
+            "card_token": "tkn_1b6fcc1045944fdfb5020a92e4fa1f91",
+            "card_issuer": "SBI",
+            "card_brand": "MASTERCARD",
+            "card_number": "XXXX-XXXXXXXX-1449",
+            "card_fingerprint": "45po8snsgehi5mestx9oi6emv6",
+            "extended_card_type": "CREDIT",
+            "card_type": "CREDIT",
+            "token": {
+                "last_four_digits": "4325",
+                "card_reference": "token_4149513e23e2e5gead99e17cdd541f10",
+                "card_fingerprint": "45po8snsgehi5mestx9oi6emv6",
+                "par": "M0010513828031519235347454746",
+                "card_isin": "510128",
+                "tokenization_status": "ACTIVE",
+                "expiry_year": "2027",
+                "expiry_month": "02",
+                "cvv_less_support": true,
+                "cvv_less_supported_gateways": [
+                    "PAYU"
+                ]
+            },
+            "provider_category": "NETWORK",
+            "card_isin": "510128",
+            "juspay_bank_code": "JP_SBI",
+            "metadata": {
+                "origin_merchant_id": "your_merchantID"
+            },
+            "tokenize_support": false,
+            "nickname": "",
+            "card_sub_type": "TITANIUM",
+            "card_exp_year": "2027",
+            "provider": "MASTERCARD",
+            "card_exp_month": "02"
+        }
+    ]
+}
