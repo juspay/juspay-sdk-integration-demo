b93af56022efbca673b1bf8e83f363faf06f2090
srikanthjuspay <152646296+srikanthjuspay@users.noreply.github.com>
2024-08-28 12:12:15 +0530

    Hdfc sample codes (#677)
    * Create SessionReq.sh
    
    Mandates request
    
    * Create Response.sh
    
    * Create orderStatus.sh

---
+++ code-samples/Mandates/Response.sh
@@ -0,0 +1,35 @@
+{
+    "status": "NEW",
+    "id": "ordeh_7198be7d5cae492ebdbacf61a0d56e82",
+    "order_id": "JP_1724825476",
+    "payment_links": {
+        "web": "https://smartgatewayuat.hdfcbank.com/payment-page/order/ordeh_7198be7d5cae492ebdbacf61a0d56e82",
+        "expiry": "2024-09-04T06:11:16Z"
+    },
+    "sdk_payload": {
+        "requestId": "9e4e4fcd48804b92aca6ce44f83e37d2",
+        "service": "in.juspay.hyperpay",
+        "payload": {
+            "action": "paymentPage",
+            "amount": "10.0",
+            "clientAuthToken": "tkn_2b94fd1556094cc49c6277870289cca9",
+            "clientAuthTokenExpiry": "2024-08-28T06:26:16Z",
+            "clientId": "hdfcmaster",
+            "collectAvsInfo": false,
+            "currency": "INR",
+            "customerId": "test9117",
+            "customerPhone": "9999999999",
+            "description": "Complete your payment",
+            "displayBusinessAs": "Test Merchant",
+            "environment": "sandbox",
+            "mandate.maxAmount": "300.00",
+            "merchantId": "testhdfc1",
+            "options.createMandate": "REQUIRED",
+            "orderId": "JP_1724825476",
+            "returnUrl": "https://www.google.com",
+            "service": "in.juspay.hyperpay"
+        },
+        "expiry": "2024-09-04T06:11:16Z"
+    },
+    "order_expiry": "2024-09-04T06:11:16Z"
+}
