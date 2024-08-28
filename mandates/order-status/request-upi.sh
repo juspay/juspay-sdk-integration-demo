fb9a308ae5dc00565574028a81940f6db412c3ad
Maheti Mehra <maheti.mehra@juspay.in>
2024-08-28 12:33:44 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference>mandates/order-status/request-upi.sh @ api-reference/docs/get-mandate-order-status by Maheti Mehra

--- mandates/order-status/request-upi.sh
+++ mandates/order-status/request-upi.sh
@@ -1,4 +1,3 @@
 curl GET 'https://api.juspay.in/orders/234823498237423894273498' \
 --header 'x-merchantid: merchant' \
 --header 'Authorization: Basic <base64 of key:>'
---header 'version: 2023-06-30'
