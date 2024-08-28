d34b271ec43d67b24c83c7fedbf441d5bdabdbec
MMaheti <maheti.mehra@juspay.in>
2024-08-28 13:06:23 +0530

    remove version (#678)

--- Wallet/update-mobile/request.sh
+++ Wallet/update-mobile/request.sh
@@ -1,6 +1,5 @@
 curl --location --request POST 'https://api.juspay.in/wallets/wlt_yvuidaupkvz3' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
---header 'version: 2023-06-30' \
 --header 'Authorization: Basic MTkx*****RGODBDNDg2Og==' \
 --data-urlencode 'command=update' \
 --data-urlencode 'mobile_number=7777777777'
