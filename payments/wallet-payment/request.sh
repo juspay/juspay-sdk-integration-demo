98fa0fab7fce5b775f13b62270e22d30fb3cad65
Priyanga7695 <99274601+Priyanga7695@users.noreply.github.com>
2024-09-21 23:19:06 +0530

    Update request.sh

--- payments/wallet-payment/request.sh
+++ payments/wallet-payment/request.sh
@@ -1,13 +1,12 @@
 curl --location 'https://api.juspay.in/txns' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: Basic xxx==' \
---data-urlencode 'order_id=de83e2f6-fa65-44b2-91f2-9af8bbbcc06b' \
---data-urlencode 'merchant_id=azharamin' \
+--data-urlencode 'merchant_id=:merchant_id' \
 --data-urlencode 'payment_method_type=WALLET' \
 --data-urlencode 'payment_method=GOPAY' \
 --data-urlencode 'redirect_after_payment=true' \
 --data-urlencode 'format=json' \
---data-urlencode 'order.order_id=1693230550' \
+--data-urlencode 'order.order_id=:order_id' \
 --data-urlencode 'order.amount=100' \
 --data-urlencode 'order.currency=IDR' \
 --data-urlencode 'order.customer_id=test123' \
