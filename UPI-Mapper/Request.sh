da2e5da3d58f77a9805b1c4647b1c21fa68f1e0a
srikanthjuspay <152646296+srikanthjuspay@users.noreply.github.com>
2024-09-26 13:35:22 +0530

    Hdfc sample codes (#714)
    * Create SessionReq.sh
    
    Mandates request
    
    * Create Response.sh
    
    * Create orderStatus.sh
    
    * Create Request.sh
    
    Adding UPI Mappet
    
    * Create VPA_Sucess.json
    
    Create VPA_Sucess.json
    
    * Create Resp_200_fail.json
    
    Create Resp_200_fail.json
    
    * Create resp_400.json

---
+++ UPI-Mapper/Request.sh
@@ -0,0 +1,4 @@
+curl --location 'https://smartgateway.hdfcbank.com/upi/get-vpa?phone_number=7869140228&merchant_id=34436' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-merchantid: 34436' \
+--header 'Authorization: Basic OTI4QjQ2REE4Mj************************UQ5Og=='
