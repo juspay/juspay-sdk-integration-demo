fb5a51aa2b3970e805faf766b8298545344c864a
Priyanga7695 <99274601+Priyanga7695@users.noreply.github.com>
2024-09-30 14:13:24 +0530

    globalordercreate.sh (#715)
    * globalordercreate.sh
    
    sea order create request
    
    * globalordercreate.sh

---
+++ mandates/order-api/globalordercreate.sh
@@ -0,0 +1,21 @@
+curl POST 'https://api.juspay.in/orders' \
+-H 'x-merchantid: merchant' \
+-H ‘'Content-Type: application/x-www-form-urlencoded'\
+-d 'order_id=234823498237423894273498' \
+-d 'amount=1.00' \
+-d 'currency=SGD' \
+-d 'customer_id=cst_lz7emoo5okgav' \
+-d 'customer_email=test@gmail.com' \
+-d 'customer_phone=9818638469' \
+-d 'options.create_mandate=REQUIRED' \
+-d 'mandate.amount_rule=VARIABLE' \
+-d 'mandate.max_amount=1000.00' \
+-d 'mandate.revokable_by_customer=true' \
+-d 'mandate.block_funds=false' \
+-d 'mandate.start_date=1598965200' \
+-d 'mandate.end_date=1914141600' \
+-d 'mandate.frequency=ASPRESENTED' \
+-d 'mandate.rule_value=1' \
+-d 'mandate.rule_type=ON' \
+-d 'gateway_id=601' \
+-d 'metadata.CHECKOUT:gateway_reference_id=entity1'
