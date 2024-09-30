f23119f69de4784ca7c331ab6948df9c759b7729
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2024-09-30 15:05:25 +0530

    Update request.sh

--- twid/ec-sdk/payment-method-api/request.sh
+++ twid/ec-sdk/payment-method-api/request.sh
@@ -1,7 +1,7 @@
 curl --location 'https://api.juspay.in/customers/<customerId>/eligibility' \
 --header 'Authorization: <Auth Key>' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
---header 'version: <current date>' \           --- This is mandatory for Twid v2
+--header 'version: <current date>' \           --- This is mandatory for Twid v2 
 --data-urlencode 'amount=100' \
 --data-urlencode 'gateway_reference_id=refId' \
 --data-urlencode 'order_id=orderId' \
