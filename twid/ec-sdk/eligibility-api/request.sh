edc48a5b47a6ef8d8a02e526dd624606a23183f3
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2024-09-30 19:38:22 +0530

    Update and rename request.json to request.sh

---
+++ twid/ec-sdk/eligibility-api/request.sh
@@ -0,0 +1,8 @@
+curl --location 'https://api.juspay.in/customers/<customerId>/eligibility' \
+--header 'Authorization: <Auth Key>' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'version: <current date>' \           --- This is mandatory for Twid v2
+--data-urlencode 'amount=100' \
+--data-urlencode 'gateway_reference_id=refId' \
+--data-urlencode 'order_id=orderId' \
+--data-urlencode 'mobile_number=mobile_number'
