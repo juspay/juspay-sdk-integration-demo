bdd256d1f9d67780f7297114753e8ec52b916312
Yashwi Shah <yashwi.shah@juspay.in>
2024-10-01 13:47:48 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference-global>orders/create-order/request.sh @ api-reference-global/docs/create-order-api by Yashwi Shah

--- orders/create-order/request.sh
+++ orders/create-order/request.sh
@@ -4,11 +4,11 @@ curl --location 'https://sandbox.juspay.in/txns' \
 --data-urlencode 'order_id=1692783824' \
 --data-urlencode 'merchant_id=paypal' \
 --data-urlencode 'payment_method_type=WALLET' \
---data-urlencode 'payment_method=GRABPAY' \
+--data-urlencode 'payment_method=PICPAY' \
 --data-urlencode 'redirect_after_payment=true' \
 --data-urlencode 'format=json'
 --data-urlencode 'order.order_id=ord_1590759422' \
 --data-urlencode 'order.amount=1015.00' \
---data-urlencode 'order.currency=SGD' \
+--data-urlencode 'order.currency=BRL' \
 --data-urlencode 'order.customer_id=cst_1590759422' \
 --data-urlencode 'order.return_url=https://merchant.in/payments' \
