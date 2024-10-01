6276107413d40ab716db451b9e4024449dcc6991
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2024-10-01 17:30:42 +0530

    Update request.sh

--- tokenization/generate-token/request.sh
+++ tokenization/generate-token/request.sh
@@ -1,10 +1,15 @@
-curl --request POST \
-     --url https://api.juspay.in/generateToken \
-     --header 'Authorization: Basic XXXXAPIKEYXXXXX' \
-     --header 'accept: application/json' \
-     --header 'authorization: Basic NxxxxxxxxxxxxxxxxxxxxkY2QUYwMzlEQ0E1QTk5Og==' \
-     --header 'content-type: application/json' \
-     --header 'x-merchantId: JuspayMID' \
-     --data 'orderData={         "consentId" : "ae3877f948bd4c95",         "customerId" : "abcd6831",         "amount" : "1",         "currency": "INR"     }' \
-     --data 'auditData={         "authRefNo" : "4c95b016def8806314bf"     }' \
-     --data service=NETWORK_TOKEN
+curl --location 'https://sandbox.juspay.in/generateToken' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Basic MDRFRDU3Mzc1Q0Q0NzMxQTMzN0QzQjJEQzlENTBCOg==' \
+--data '{
+    "service" : "NETWORK_TOKEN",
+    "cardData": "xxxxxxx",
+    "orderData" : {
+        "consentId" : "xxxxxxx",
+        "customerId" : "xxxxxxx"
+    },
+    "audit": {
+        "authRefNo" : "test12324"
+    },
+    "shouldSendToken": true
+}'
