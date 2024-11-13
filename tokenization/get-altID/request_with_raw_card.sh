ab61b247cd0660e9e6150fc63020333fc5abd8c5
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2024-11-13 11:16:11 +0530

    Create request_with_raw_card.sh

---
+++ tokenization/get-altID/request_with_raw_card.sh
@@ -0,0 +1,15 @@
+curl --location 'https://sandbox.juspay.in/get/altId' \
+--header 'accept: application/json' \
+--header 'content-type: application/json' \
+--header 'x-merchantId: testmerchjapnit' \
+--header 'Authorization: ••••••' \
+--data '{
+    "cardData" : "eyJhbGciOiJSU0EtT0FFUC0yNTYiLCJlbmMiOiJBMTI4R0NNIiwia2lkIjoiMjIxODE2OGMtZWU2Yi00OWQ3LWE4NTEtOGI5NGU3ZDAwNjhkIn0.k2dNpL29TyUstnZTmpDsFcqgMBeehj1kiDwJ7ILY--cRlhNB3ouX-xsPjIlMbWWPzn9vdc5xVUdzmVZM_Uergrw223aTRGKtUbmJIMmiJh1w_4nz4gkumPJACftN49jQ2eQQCvJvQOBGi3mNS8XBf8o_NrApKB4FITZs3wN8sNAeU9lSF5C7b70w-EyTJk0cWtedV0bow2Hr7f0hkz5APVTPV39vd4CTQ_awRSH5d1pkzBJ16hus4pT-oKcQFF7oKyqjqpeeU3IxNFI0Jugpk2Px98Lfknw3RdXdmSSIuMPnTLtfufnd_ApkrPrGOzdFupeXUTJf3JVmTRwRjCKP9o1WBUw.81NIEQ0I_iZbI0Ac.jT2M_kG-zdmP5EB_p_gORkqwxOFboac64z-Bhctn-3yu1da4vVGI0m7D9gr5_7hvJImtmHSdWZP1AupEadD1jgyRq8DmEM1lb1UuIwXhh50vlhJRWbGC_yabIOQhECWl89xWIVI5ui_J-TtFqdFpbBk.4p9d_CY36ix5WMGn6f_AEw",
+    "orderData": {
+        "amount": 1.0,
+        "currency": "INR"
+    },
+    "correlationId": "GTRHegrgYTJ",
+    "keyId": "key_40662ff7908349fbb0tgh584f81b18c2369",
+    "shouldSendRawCard": "true"
+}
