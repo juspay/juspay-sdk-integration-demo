de03e1dc37eb9e96fd9a993d335e7aac6287a031
aklankjuspay <168088064+aklankjuspay@users.noreply.github.com>
2024-11-15 18:01:02 +0530

    Create session_normal_tpv.sh

---
+++ api-reference/mandates/mutual_fund/session_normal_tpv.sh
@@ -0,0 +1,19 @@
+curl --location 'https://api.juspay.in/session' \
+--header 'Authorization: Basic NjQ4RTZjc0=' \
+--header 'x-merchantid: test' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "order_id": "06022023",
+    "amount": "2.0",
+    "customer_id": "cst_3uh6gumy3esndhdr",
+    "customer_email": "test@mail.com",
+    "customer_phone": "9876543210",
+    "payment_page_client_id": "test",
+    "currency": "INR",
+    "action": "paymentPage",
+    "return_url": "https://www.test.com",
+    "order_type":"TPV_PAYMENT",
+    "mutual_fund_details": "[{\"memberId\": \"ABCDE\", \"userId\": \"ABCDEFGHIJ\", \"mfPartner\": \"BSE\", \"orderNumber\": \"1727380577\", \"amount\": \"1\", \"investmentType\": \"SIP\", \"schemeCode\": \"LT\",\"folioNumber\": \"190983010\",\"amcCode\": \"UYTIUI\",\"panNumber\": \"TYLIO7823U\"}, {\"memberId\": \"ABCDE\", \"userId\": \"ABCDEFGHIJ\", \"mfPartner\": \"BSE\", \"orderNumber\": \"1727380577-1\", \"amount\": \"1\", \"investmentType\": \"SIP\", \"schemeCode\": \"LT\",\"folioNumber\": \"190983010\",\"amcCode\": \"UYTIUI\",\"panNumber\": \"TYLIO7823U\"}]",
+    "gateway_id":"514",
+    "metadata.bank_account_details" : "[{\"bank_account_number\":\"83748239234872\",\"bank_ifsc\":\"AABD0000011\", \"bank_beneficiary_name\" : \"Abc\"}]",
+}'
