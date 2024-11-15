106d71b8b63213708871f26a68fa2f14998048df
aklankjuspay <168088064+aklankjuspay@users.noreply.github.com>
2024-11-15 18:00:29 +0530

    Create session_mandate.sh

---
+++ api-reference/mandates/mutual_fund/session_mandate.sh
@@ -0,0 +1,19 @@
+curl --location 'https://sandbox.juspay.in/session' \
+--header 'Authorization: Basic NjQ4RTZGMTQzQjc0=' \
+--header 'x-merchantid: test' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "order_id": "06022023",
+    "amount": "2.0",
+    "customer_id": "cst_3uh6gumy3esndhdr",
+    "customer_email": "test@mail.com",
+    "customer_phone": "9876543210",
+    "payment_page_client_id": "test",
+    "currency": "INR",
+    "action": "paymentPage",
+    "return_url": "https://www.google.com",
+    "mutual_fund_details": "[{\"memberId\": \"ABCDE\", \"userId\": \"ABCDEFGHIJ\", \"mfPartner\": \"BSE\", \"orderNumber\": \"1727380577\", \"amount\": \"1\", \"investmentType\": \"SIP\", \"schemeCode\": \"LT\",\"folioNumber\": \"190983010\",\"amcCode\": \"UYTIUI\",\"panNumber\": \"TYLIO7823U\"}, {\"memberId\": \"ABCDE\", \"userId\": \"ABCDEFGHIJ\", \"mfPartner\": \"BSE\", \"orderNumber\": \"1727380577-1\", \"amount\": \"1\", \"investmentType\": \"SIP\", \"schemeCode\": \"LT\",\"folioNumber\": \"190983010\",\"amcCode\": \"UYTIUI\",\"panNumber\": \"TYLIO7823U\"}]",
+    "gateway_id":"514",
+    "options.create_mandate":"REQUIRED",
+    "mandate_max_amount": "2000"
+}'
