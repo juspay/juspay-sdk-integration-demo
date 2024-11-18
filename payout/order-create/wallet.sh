7d9e4c0c9f8f36f51d9f01591750b82ffe52a3e8
tusharjuspay <tushar.verma@juspay.in>
2024-11-18 17:58:07 +0530

    Brazil Doc Payout Request and Response (#759)
    * Update wallet.sh
    
    Brazil Doc Payout
    
    * Update wallet.json
    
    * Update request.sh
    
    * Update response.json
    
    * Update request.sh
    
    * Update response.json
    
    * Create wallet.sh
    
    * Create wallet.json

--- payout/order-create/wallet.sh
+++ payout/order-create/wallet.sh
@@ -1,27 +1,41 @@
-curl --location --request POST 'https://api.juspay.in/payout/merchant/v1/orders/' \
--u your_api_key: \
+curl --location --request POST 'https://sandbox.juspay.in/payout/merchant/v1/orders' \
 --header 'Content-Type: application/json' \
---header 'x-merchantid: <merchant-id>' \
+--header 'x-merchantid: <merchant id>' \
+--header 'Authorization: Basic (b64 encoded API key)' \
 --data-raw '{
-    "orderId": "Pjl95dMQh2v2ysT1",
+    "orderId": "ONaajHngrONssVSu",
     "fulfillments": [
-        {
-            "preferredMethodList": [
-            ],
-            "amount": 1,
-            "beneficiaryDetails": {
-                "details": {
-                    "name": "Taral Shah",
-                    "brand": "MERCADOPAGO",
-                    "walletIdentifier" : "99999999"
-                },
-                "type": "WALLET"
-            }
-        }
-    ],
-    "amount": 1,
-    "customerId": "m123",
-    "customerPhone": "99999999",
-    "customerEmail": "shubham@gmail.com",
-    "type": "FULFILL_ONLY"
+       {
+           "preferredMethodList": [
+               "PAGSEGURO_WALLET"
+           ],
+           "amount": 1,
+           "beneficiaryDetails": {
+               "details": {
+                   "walletIdentifier" : "a1f4102e-a446-4a57-bcce-6fa48899c1d2",
+                   "brand" : "PIX_KEY",
+                   "payeeAdditionalDetails" : {
+                       "documents" : [
+                           {
+                               "docId" : "853.513.468-93",
+                               "docType" : "CNPJ"
+                           }
+                       ],
+                       "country" : "BRA",
+                       "name" : "Tushar"
+                   }                
+               },
+               "type": "WALLET"
+           },
+           "additionalInfo" : {
+               "transactionType" : "B2C"
+           },
+           "fulfillmentCurrency" : "BRL"
+       }
+   ],
+   "amount": 1,
+   "customerId": "dsgsdgfsdgfsg",
+   "customerPhone": "11987654321",
+   "customerEmail": "name@gmail.com",
+   "type": "FULFILL_ONLY"
 }'
