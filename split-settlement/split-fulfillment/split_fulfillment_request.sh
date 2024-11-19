e5b4342f7e7e1ea408dc21dd50fd0370af61114b
Simranshaik1 <122527232+Simranshaik1@users.noreply.github.com>
2024-11-19 15:26:09 +0530

    Update split_fulfillment_request.sh (#760)
    * Update split_fulfillment_request.sh
    
    * Create split_fulfillment_error_response.json

--- split-settlement/split-fulfillment/split_fulfillment_request.sh
+++ split-settlement/split-fulfillment/split_fulfillment_request.sh
@@ -1,8 +1,7 @@
-curl --location 'https://sandbox.juspay.in/orders/order1730187936/fulfillment' \
+curl --location 'https://sandbox.juspay.in/orders/yourUniqueOrderId/fulfillment' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
---header 'Postman-Token: 98866fa8-de08-4b18-b46e-1b0e73f2b00d' \
 --header 'cache-control: no-cache' \
---header 'Authorization: Basic QzQ3NEE3MjYyQ0M0NjhCQTYzQzIwNDVFRUM3RDgwOg==' \
+--header 'Authorization: Basic base64encodedkey==' \
 --data-urlencode 'fulfillment_status=SUCCESS' \
 --data-urlencode 'fulfillment_command=POST_TXN_SPLIT_SETTLEMENT' \
 --data-urlencode 'split_settlement_details={"marketplace":{"amount":2},"mdr_borne_by":"ALL","vendor":{"split":[{"amount":3,"merchant_commission":1,"sub_mid":"Vendor1"},{"amount":3,"merchant_commission":1,"sub_mid":"Vendor2"}]}}'
