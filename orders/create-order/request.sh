dd8b044621cc023836759c1e258dcce932ce3fd1
yashwis <yashwi.shah@juspay.in>
2024-11-26 18:04:15 +0530

    brazil changes

--- orders/create-order/request.sh
+++ orders/create-order/request.sh
@@ -1,14 +1,10 @@
-curl --location 'https://sandbox.juspay.in/txns' \
+curl --location 'https://sandbox.juspay.in/orders' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
---header 'Authorization: Basic e3thcGlrZXl9fTo=' \
+--header 'Authorization: Basic OTgwZERERCOg==' \
 --data-urlencode 'order_id=1692783824' \
---data-urlencode 'merchant_id=paypal' \
---data-urlencode 'payment_method_type=WALLET' \
---data-urlencode 'payment_method=GRABPAY' \
---data-urlencode 'redirect_after_payment=true' \
---data-urlencode 'format=json'
---data-urlencode 'order.order_id=ord_1590759422' \
---data-urlencode 'order.amount=1015.00' \
---data-urlencode 'order.currency=BRL' \
---data-urlencode 'order.customer_id=cst_1590759422' \
---data-urlencode 'order.return_url=https://merchant.in/payments' \
+--data-urlencode 'return_url=https://merchant.in/payments' \
+--data-urlencode 'amount=100' \
+--data-urlencode 'metadata.PAYPAL%3Agateway_reference_id=' \
+--data-urlencode 'gateway_id=20' \
+--data-urlencode 'currency=BRL'
+
