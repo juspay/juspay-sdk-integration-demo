6faddd0119130c7912956c3bb68a44da55dcd27b
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2024-11-27 17:50:43 +0530

    Update request.sh

--- orders/orderFulfilment/request.sh
+++ orders/orderFulfilment/request.sh
@@ -1,9 +1,9 @@
 curl --location -g --request POST 'https://api.juspay.in/orders/{order_id}/fulfillment' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
---header 'Authorization: <Your Base64 Encoded API Key>' \
+--header 'Authorization: Basic <Your Base64 Encoded API Key>' \
 --header 'x-merchantid: merchant_id' \
 --data-urlencode 'fulfillment_status=SUCCESS' \
 --data-urlencode 'fulfillment_command=NO_ACTION' \
 --data-urlencode 'fulfillment_time=2024-07-08T16:30:33' \
---data-urlencode 'fulfillment_id=<Committed PNR>' \
+--data-urlencode 'fulfillment_id=your_fulfillment_id_012' \
 --data-urlencode 'fulfillment_data=test_data_nvewnvubbewnv'
