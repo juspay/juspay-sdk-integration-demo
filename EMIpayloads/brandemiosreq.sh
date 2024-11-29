52e998fcf2d6b824dee43e6d28834cd32b54ba33
Priyanga7695 <99274601+Priyanga7695@users.noreply.github.com>
2024-11-29 15:48:30 +0530

    brandemiosreq.sh

--- EMIpayloads/brandemiosreq.sh
+++ EMIpayloads/brandemiosreq.sh
@@ -1,4 +1,4 @@
-curl GET 'https://api.juspay.in/orders/123test' \
+curl GET 'https://api.juspay.in/orders/6473eaec57f56362a73' \
 --header 'x-merchantid: merchant' \
 --header 'Authorization: Basic <base64 of key:>'
 --header 'version: 2023-06-30'
