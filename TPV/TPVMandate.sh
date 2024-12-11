0d82ca13c3afcb2599cecf5845025224613f289a
srikanthjuspay <152646296+srikanthjuspay@users.noreply.github.com>
2024-12-11 11:58:07 +0530

    Adding TPV Code snippets (#808)
    * Update request-upi.sh
    
    * Update request.sh
    
    * Update request.sh
    
    * Update request-upi.sh
    
    * Update upi.json
    
    * Update saved-card.json
    
    * Create TPV.sh
    
    * Create TPVResp.json
    
    * Create TPVMandate.sh
    
    * Create TPVMandateResp.json
    
    * Create TranTPV.sh
    
    * Create TranTPVResp.json
    
    * Create TranTPVmandarte.sh
    
    * Create TranTPVResp.sh

---
+++ TPV/TPVMandate.sh
@@ -0,0 +1,20 @@
+curl --location 'https://smartgatewayuat.hdfcbank.com/session' \
+--header 'Content-Type: application/json' \
+--header 'x-merchantid: <merchant_id>' \
+--header 'x-customerid: CUST001' \
+--header 'Authorization: Basic base_64_encoded_api_key==' ' \
+--data-raw '{
+    "order_id": "dedSddfd4",
+    "amount": "1",
+    "customer_id": "CUST001",
+    "customer_email": "rahuls@gmail.in",
+    "customer_phone": "8604613494",
+    "payment_page_client_id": "hdfcmaster",
+    "action": "paymentPage",
+    "currency": "INR",
+    "return_url": "https://www.google.com",
+    "order_type" : "TPV_PAYMENT",
+  "metadata.bank_account_details" : "[{\"bank_account_number\":\"83748239234872\",\"bank_ifsc\":\"HDFC0876543\"}]",
+  "options.create_mandate" : "REQUIRED",
+  "mandate.max_amount" : "300"
+}'
