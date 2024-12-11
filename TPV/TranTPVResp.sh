754675d565c6b302bae22f02927a30a1b43572c5
srikanthjuspay <152646296+srikanthjuspay@users.noreply.github.com>
2024-12-11 11:18:40 +0530

    Create TranTPVResp.sh

---
+++ TPV/TranTPVResp.sh
@@ -0,0 +1,12 @@
+{
+    "status": "CREATED",
+    "status_id": 1,
+    "id": "ordeh_9d1cbcbcc0ad41058df6c14384ce155c",
+    "order_id": "TPVTEST14",
+    "payment_links": {
+        "mobile": "https://smartgatewayuat.hdfcbank.com/payment-page/order/ordeh_9d1cbcbcc0ad41058df6c14384ce155c",
+        "web": "https://smartgatewayuat.hdfcbank.com/payment-page/order/ordeh_9d1cbcbcc0ad41058df6c14384ce155c",
+        "iframe": "https://smartgatewayuat.hdfcbank.com/payment-page/order/ordeh_9d1cbcbcc0ad41058df6c14384ce155c"
+    },
+    "order_expiry": "2024-12-18T05:42:58Z"
+}
