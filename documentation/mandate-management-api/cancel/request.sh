311723988a58ddf96e09c2dc776f5d3a3a60f8e8
Bose As <bose.as@juspay.in>
2024-12-12 11:07:03 +0530

    Mandate management API

---
+++ documentation/mandate-management-api/cancel/request.sh
@@ -0,0 +1,2 @@
+curl --location --request PATCH 'https://api-test.lotuspay.com/api/mandates/cancel?mandate_id=MD0086WWA8U5CH' \
+--header 'Authorization: Basic c2tfdGVzdF82MXFIUURuYTgzMWNOTm9vb29uRzFlQWlEOg==' 
