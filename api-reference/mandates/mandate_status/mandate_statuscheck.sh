637dd1b631756ca42bb6b1955dfbd0e0636fd83c
harshiniallada14 <harshini.allada@juspay.in>
2024-12-13 16:24:06 +0530

    Create mandate_statuscheck.sh

---
+++ api-reference/mandates/mandate_status/mandate_statuscheck.sh
@@ -0,0 +1,4 @@
+curl --location 'https://sandbox.hyperpg.in/mandates/hyhTD5Ga4jJ2KXZwwnyh4A' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'Authorization: your api key' \
+--data-urlencode 'command=check_status'
