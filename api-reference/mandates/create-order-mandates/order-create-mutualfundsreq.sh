d2529e517ba1d111d45735e59c96a37873983c6a
harshiniallada14 <harshini.allada@juspay.in>
2024-12-13 12:46:40 +0530

    Patch 6 (#817)
    * Create nb-post-response.json
    
    * Create nb-get-response.json
    
    * Create ordercreatereq.sh
    
    * Create order-create-mutualfundsreq.sh
    
    * Create order-refund-post-mandatereq.sh
    
    * Create createorderres.json

---
+++ api-reference/mandates/create-order-mandates/order-create-mutualfundsreq.sh
@@ -0,0 +1,12 @@
+curl --location 'http/api.hyperpg.in/ecr/orders' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'version: 2020-02-17' \
+--header 'x-merchantid: azhar_test' \
+--header 'Authorization: Basic N0JBRDA4OUExRjYwNEYwREEyNDlCNjY3QTVCMUQzM0Q6' \
+--data-urlencode 'order_id=HS1720178554asds' \
+--data-urlencode 'amount=1600' \
+--data-urlencode 'currency=INR' \
+--data-urlencode 'customer_phone=8467359820' \
+--data-urlencode 'mutual_fund_details=[{"memberId":"ABCDE","userId":"ABCDEFGHIJ","mfPartner":"BSE","folioNumber":"190983010","orderNumber":"order_zer12345ssss","amount":"800","schemeCode":"LT","amcCode":"UYTIUI","panNumber":"TYLIO7823U","investmentType":"LUMPSUM"},{"memberId":"ABCDE","userId":"ABCDEFGHIJ","mfPartner":"BSE","folioNumber":"190983010","orderNumber":"order_zer12345sssss","amount":"800.00","schemeCode":"LT","amcCode":"UYTIUI","panNumber":"TYLIO7823U","investmentType":"LUMPSUM"}]' \
+--data-urlencode 'options.create_mandate=REQUIRED' \
+--data-urlencode 'mandate.max_amount=10'
