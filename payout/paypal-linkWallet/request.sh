bf4d5eaacc2b35053571f0e602999a2246fac754
tusharjuspay <tushar.verma@juspay.in>
2024-12-17 15:14:31 +0530

    Update paypal linkwallet api request

--- payout/paypal-linkWallet/request.sh
+++ payout/paypal-linkWallet/request.sh
@@ -1,9 +1,8 @@
-curl --request GET \ 
---url https://api.juspay.io/payout/client/v1/linkwallet \
+curl --request POST \ 
+--url https://api.juspay.io/payout/merchant/v1/linkwallet \
 --header 'x-merchantid: <merchant id>' \
 --header 'Content-Type: application/json' \
 --data-raw '{
-    "client_auth_token" :"9b74d1f4cb347ac964f5587a2e01bf100016",
     "merchantReturnUrl" : "https://www.abc.com/",
     "customerId" : "juspaycustomer1",
     "customerPhone" : "9475839573",
