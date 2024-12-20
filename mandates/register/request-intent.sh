f76cb55220b12bd1fc91640de8c80f4e5c4247bc
srikanthjuspay <152646296+srikanthjuspay@users.noreply.github.com>
2024-12-20 19:00:45 +0530

    Update request-intent.sh

--- mandates/register/request-intent.sh
+++ mandates/register/request-intent.sh
@@ -1,12 +1,13 @@
-curl --location --request POST 'https://smartgatewayuat.hdfcbank.com/txns' \
+curl --location 'https://smartgatewayuat.hdfcbank.com/txns' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
---data-urlencode 'order_id=DW-1dvn6nL9AD' \
---data-urlencode 'merchant_id=merchantId' \
---data-urlencode 'should_create_mandate=true' \
+--header 'x-merchantid: merchant_id' \
+--data-urlencode 'order_id=TEST23' \
+--data-urlencode 'merchant_id=merchant_id' \
 --data-urlencode 'payment_method_type=UPI' \
---data-urlencode 'payment_method=UPI_PAY' \
+--data-urlencode 'payment_method=UPI_PAY \
 --data-urlencode 'txn_type=UPI_PAY' \
 --data-urlencode 'sdk_params=true' \
---data-urlencode 'mandate_type=EMANDATE'\
+--data-urlencode 'should_create_mandate=true' \
+--data-urlencode 'mandate_type=EMANDATE' \
 --data-urlencode 'redirect_after_payment=true' \
---data-urlencode 'format=json' 
+--data-urlencode 'format=json'
