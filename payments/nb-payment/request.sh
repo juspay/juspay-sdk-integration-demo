95d20b0bfcde4e69cf21d8dbb953c7df44ba993e
srikanthjuspay <152646296+srikanthjuspay@users.noreply.github.com>
2024-12-20 18:57:04 +0530

    Update request.sh

--- payments/nb-payment/request.sh
+++ payments/nb-payment/request.sh
@@ -1,9 +1,9 @@
-curl -X POST https://smartgateway.hdfcbank.com/txns \
---header 'Authorization: Basic QTA0QT*******ODg1Og==' \
---header 'Content-Type: application/x-www-form-urlencoded' 
--d "order_id=:order_id" \
--d "merchant_id=:merchant_id" \
--d "payment_method_type=NB" \
--d "payment_method=NB_ICICI" \
--d "redirect_after_payment=true" \
--d "format=json"
+curl --location 'https://smartgatewayuat.hdfcbank.com/txns' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-merchantid: merchant_id' \
+--data-urlencode 'order_id=TEST23' \
+--data-urlencode 'merchant_id=merchant_id' \
+--data-urlencode 'payment_method_type=NB' \
+--data-urlencode 'payment_method=NB_ICICI' \
+--data-urlencode 'redirect_after_payment=true' \
+--data-urlencode 'format=json'
