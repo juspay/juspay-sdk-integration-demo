7879804a8abe571e6d9276e1b379ac826dd60ed5
Subhash Chandra <subhashchandra30720@gmail.com>
2025-01-24 13:24:51 +0530

    updated service from hyperapi to in.juspay.hyperapi

--- sample-payload/third-party/amazon-upi/request.sh
+++ sample-payload/third-party/amazon-upi/request.sh
@@ -1,6 +1,6 @@
 {
     "requestId": "7bd92330-2c0f-4dbc-91d0-8f2484910b40",
-    "service": "hyperapi",
+    "service": "in.juspay.hyperapi",
     "payload": {
         "action": "upiTxn",
         "orderId": "hyperorder95862",
