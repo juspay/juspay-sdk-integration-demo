cd4133a1d9da9a33fc65d2e6b97b5250905d5a3c
srikanthjuspay <152646296+srikanthjuspay@users.noreply.github.com>
2025-03-08 20:28:33 +0530

    Create capture.sh
    adding capture

---
+++ code-samples/AuthCapture/capture.sh
@@ -0,0 +1,3 @@
+curl --location --request POST 'https://smartgateway.hdfcbank.com/v2/txns/:txnUUID/capture' \
+--header 'accept: application/json' \
+--header 'Authorization: Basic <Base64EncodedAPIKEY>'
