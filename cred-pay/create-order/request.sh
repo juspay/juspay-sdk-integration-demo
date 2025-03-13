19170e93a68b03a1e2d69f90458f6a6d1729120b
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:44:11 +0530

    Update request.sh

--- cred-pay/create-order/request.sh
+++ cred-pay/create-order/request.sh
@@ -1,5 +1,6 @@
 curl --location --request POST 'https://api.juspay.in/orders' \
 --header 'Authorization: Basic <API_KEY>' \
+--header 'x-routing-id: customer_1122'\
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'order_id=Test1608304994' \
 --data-urlencode 'amount=100'
