770d25f4e6ca779b3978b02d03aa84dfb85c99a9
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:49:27 +0530

    Update request-gateway-data.sh

--- Wallet/eligibility/request-gateway-data.sh
+++ Wallet/eligibility/request-gateway-data.sh
@@ -1,5 +1,6 @@
 curl --location --request POST 'https://api.juspay.in/customers/testcustomer/eligibility' \
 --header 'Authorization: Basic NTlEQzNG****QzE1Og==' \
+--header 'x-routing-id: customer_1122'\
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'amount=1' \
 --data-urlencode 'gateway_data={"SIMPL" : {"payload":"rtyuyhgrfde34r5ty6u7i765gty"}'
