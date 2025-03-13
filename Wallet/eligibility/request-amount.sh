a358bc5f8dfeb1c47c5c8d1f9686970fc58acec7
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:49:01 +0530

    Update request-amount.sh

--- Wallet/eligibility/request-amount.sh
+++ Wallet/eligibility/request-amount.sh
@@ -1,5 +1,6 @@
 curl -X POST \
   https://api.juspay.in/customers/testcustomer/eligibility \
   -H 'Authorization: Basic NkY4Q****MzNzQ1Og==' \
+  -H 'x-routing-id: customer_1122'\
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -d 'amount=1'
