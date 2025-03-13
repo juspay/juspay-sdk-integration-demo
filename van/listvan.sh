e91a778dcab3264fb262b9f83f5122c6e15ad623
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 20:11:47 +0530

    Update listvan.sh

--- van/listvan.sh
+++ van/listvan.sh
@@ -1,4 +1,5 @@
 curl --location 'https://sandbox.juspay.in/customers/customers/:customer_id/virtual-accounts' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-routing-id: customer_1122'\
 --header 'x-merchantid: merchant_id'\
 --header 'Authorization: Basic RjMxREI*********5Q0I0NjMyOg=='
