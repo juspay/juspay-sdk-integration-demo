b1a95f0951b2fcde274db809106d45a32959d08e
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:41:47 +0530

    Update capture.sh

--- pre-auth-capture/capture.sh
+++ pre-auth-capture/capture.sh
@@ -1,4 +1,5 @@
 curl --request POST \
      --url https://api.juspay.in/v2/txns/txn_uuid/capture \
      --header 'accept: application/json' \
+     --header 'x-routing-id: customer_1122'\
      --header 'authorization: Basic OUU4QxxxxxxxxxxxxUJDQTQzMEM2RkFDMjcyQjM5Og=='
