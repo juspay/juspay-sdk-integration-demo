5aea89a1a5004b608bb357763419a5ad6c18de95
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 20:10:35 +0530

    Update Account Creation.sh

--- payout-api/virtual-account/Account Creation.sh
+++ payout-api/virtual-account/Account Creation.sh
@@ -1,5 +1,6 @@
 curl --location 'https://api.juspay.in/customers/:customer_id/virtual-accounts' \
 --header 'Content-Type: application/json' \
+--header 'x-routing-id: customer_1122'\
 --header 'Authorization: Basic SlVTUEFZOmVkYzEXXXXXXXXXXExOGExMjRjMA==' \
 --data '{
     "virtual_account_reference": "vaRef1",
