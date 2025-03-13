4b3f4d7a6ca8e48f20805c052174b430ead63b54
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:16:02 +0530

    Update request-card.sh

--- mandates/order-status/request-card.sh
+++ mandates/order-status/request-card.sh
@@ -1,5 +1,6 @@
 curl GET \
  {host}/orders/635 \
 --header 'x-merchantid: merchant' \
+--header 'x-routing-id: customer_1122'\
 --header 'Authorization: Basic <base64 of key:>'
 --header 'version: 2023-06-30'
