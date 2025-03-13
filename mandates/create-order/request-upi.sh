3352548f0ef795ef88ef7e329a0af8097d74bc32
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:09:36 +0530

    Update request-upi.sh

--- mandates/create-order/request-upi.sh
+++ mandates/create-order/request-upi.sh
@@ -1,6 +1,7 @@
 curl POST 'https://api.juspay.in/orders' \
 -H 'x-merchantid: merchant' \
 -H 'Content-Type: application/x-www-form-urlencoded'\
+-H 'x-routing-id: customer_1122'\
 -d 'order_id=234823498237423894273498' \
 -d 'amount=1.00' \
 -d 'currency=INR' \
