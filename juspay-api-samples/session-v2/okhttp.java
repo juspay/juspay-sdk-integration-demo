1f39853efa9f5e479c2c0cfd557caf1f6e322403
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 21:19:49 +0530

    Update okhttp.java

--- juspay-api-samples/session-v2/okhttp.java
+++ juspay-api-samples/session-v2/okhttp.java
@@ -7,7 +7,7 @@ public JSONObject createSession() {
     String apiKey = "<your_api_key>";
     String clientId = "<your_client_id>";
     String merchantId = "<your_merchant_id>";
-
+    String xRoutingId = "<your_routing_id>";
 
     payload.put("order_id", "testing-order-id-four");
     payload.put("amount", "1.0");
@@ -37,6 +37,7 @@ public JSONObject createSession() {
             .addHeader("x-merchantid", merchantId)
             .addHeader("Authorization", authorization)
             .addHeader("Content-Type", "application/json")
+            .addHeader("x-routing-id", xRoutingId)
             .build();
     Response response = okHttpClient.newCall(request).execute();
     JSONObject responseJSON = new JSONObject(response.body().string());
