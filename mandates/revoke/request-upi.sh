ae8d9c76dd46f784d641c8b943acfe2d9ee9acc8
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:19:28 +0530

    Update request-upi.sh

--- mandates/revoke/request-upi.sh
+++ mandates/revoke/request-upi.sh
@@ -1,4 +1,5 @@
 curl POST 'https://api.juspay.in/mandates/7SucJZ2PCyoGY5vvW8Xa26' \
 -H 'x-merchantid: merchant' \
 -H 'Authorization: Basic <Base-64 Key>' 
+-H 'x-routing-id: customer_1122'\
 -d 'command=revoke'
