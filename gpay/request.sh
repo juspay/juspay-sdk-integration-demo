f1c883941639a8be4d9f0aa42eed51f450accd71
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:40:21 +0530

    Update request.sh

--- gpay/request.sh
+++ gpay/request.sh
@@ -2,4 +2,5 @@ curl --request POST \
      --url https://api.juspay.in/txns \
      --header 'accept: application/json' \
      --header 'authorization: Basic OUU4QkxxxxxxxxxxxxxxxTQzMEM2RkFDMjcyQjM5Og==' \
+     --header 'x-routing-id: customer_1122'\
      --header 'content-type: application/x-www-form-urlencoded'
