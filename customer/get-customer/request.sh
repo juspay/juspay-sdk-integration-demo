1e0b721bbce33a65c3fd0692375faba221b9a9c0
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:16:05 +0530

    Update request.sh

--- customer/get-customer/request.sh
+++ customer/get-customer/request.sh
@@ -2,4 +2,5 @@ curl -X GET \
   https://api.juspay.in/customers/:customerid \
   -u your_api_key:  \
   -H 'x-merchantid: merchant_id'\
+  -H 'x-routing-id: customer_1122'\
   -H 'Content-Type: application/x-www-form-urlencoded'\
