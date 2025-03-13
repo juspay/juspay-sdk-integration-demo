5953680f7fdbeb6848770a6f68f3f11421184e87
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:16:13 +0530

    Update request-nb.sh

--- mandates/order-status/request-nb.sh
+++ mandates/order-status/request-nb.sh
@@ -1,5 +1,6 @@
 curl GET \
  {host}/orders/ORD1622098688 \
 --header 'x-merchantid: merchantId' \
+--header 'x-routing-id: customer_1122'\
 --header 'Authorization: Basic <base64 of key:>'
 --header 'version: 2023-06-30'
