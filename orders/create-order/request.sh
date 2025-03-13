3e3845e427f1d09dd99a1a9af21504f8753c0d11
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:20:17 +0530

    Update request.sh

--- orders/create-order/request.sh
+++ orders/create-order/request.sh
@@ -1,6 +1,7 @@
 curl --location --request POST 'https://api.juspay.in/orders' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'x-merchantid: merchant_id'\
+--header 'x-routing-id: customer_1122'\
 --header 'Authorization: Basic QTA0QT*******ODg1Og==' \
 --data-urlencode 'order_id=14183944763' \
 --data-urlencode 'amount=100.00' \
