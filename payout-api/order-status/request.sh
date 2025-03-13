c5a0543c7474245eb907a96bfce970f170883df3
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 20:52:40 +0530

    Update request.sh

--- payout-api/order-status/request.sh
+++ payout-api/order-status/request.sh
@@ -1,4 +1,5 @@
 curl --location 'https://sandbox.juspay.in/payout/merchant/v1/orders/:orderId?expand=fulfillment%2Cpayment%2Crefund' \
 --header 'Content-Type: application/json' \
+--header 'x-routing-id: customer_1122'\
 --header 'x-merchantid: <merchant id>' \
 --header 'Authorization: Basic (b64 encoded API key)'
