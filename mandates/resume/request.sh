1e01c74cdbdb6b8ec11dd4498e6894ed75a87969
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:21:37 +0530

    Update request.sh

--- mandates/resume/request.sh
+++ mandates/resume/request.sh
@@ -1,5 +1,6 @@
 curl --location --request POST 'http://api.juspay.in/mandates/7KKT3yu3LcQjskY5F4moS5M' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: Basic QjU3Qxxxxxxxxyuegj2ehkQ0MTg4N0NEMDU6' \
+--header 'x-routing-id: customer_1122'\
 --data-urlencode 'command=resume' \
 --data-urlencode 'resume_date=1696032000'
