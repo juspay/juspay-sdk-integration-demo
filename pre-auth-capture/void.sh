218e5acfa5b3a9496bc424890ce524911d77ac50
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:42:03 +0530

    Update void.sh

--- pre-auth-capture/void.sh
+++ pre-auth-capture/void.sh
@@ -1,4 +1,5 @@
 curl --request POST \
      --url https://api.juspay.in/v2/txns/txn_uuid/void \
      --header 'accept: application/json' \
+     --header 'x-routing-id: customer_1122'\
      --header 'authorization: Basic OUU4QxxxxxxxxxxxxxxxxTQzMEM2RkFDMjcyQjM5Og=='
