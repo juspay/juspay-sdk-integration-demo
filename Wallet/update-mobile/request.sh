40ce2b5757eede783eb5129ca56555c6bb6068ee
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:47:37 +0530

    Update request.sh

--- Wallet/update-mobile/request.sh
+++ Wallet/update-mobile/request.sh
@@ -1,5 +1,6 @@
 curl --location --request POST 'https://api.juspay.in/wallets/wlt_yvuidaupkvz3' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: Basic MTkx*****RGODBDNDg2Og==' \
+--header 'x-routing-id: customer_1122'\
 --data-urlencode 'command=update' \
 --data-urlencode 'mobile_number=7777777777'
