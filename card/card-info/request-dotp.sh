490db6c4f30cf7b87dfc20239213ba0cb4de82d9
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:37:43 +0530

    Update request-dotp.sh

--- card/card-info/request-dotp.sh
+++ card/card-info/request-dotp.sh
@@ -1,2 +1,3 @@
 curl -X GET https://api.juspay.in/cardbins/405028700?merchant_id=merchantId&options.check_direct_otp_support=true' \
--u your_api_key: 
+-u your_api_key: \
+-H 'x-routing-id: customer_1122'
