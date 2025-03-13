d272a7ab419b893c0c6427f2efd1c73b6689cc39
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:08:17 +0530

    Update intent.sh

--- upi/intent.sh
+++ upi/intent.sh
@@ -1,4 +1,5 @@
 curl -X POST https://api.juspay.in/txns \
+-H 'x-routing-id: customer_1122'\
 -H 'Content-Type: application/x-www-form-urlencoded' \
 -d 'merchant_id=juspay&order_id=9727125664Q20200107180320&payment_method_type=
 UPI&payment_method=UPI&txn_type=UPI_PAY&redirect_after_payment=true& format=json&sdk_params=true'
