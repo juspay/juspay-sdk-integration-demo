25d207ed76c648cbb3f37b5bc13aa394ebb1bdbc
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:35:52 +0530

    Update request.sh

--- card/update-card/request.sh
+++ card/update-card/request.sh
@@ -1,5 +1,6 @@
 curl -X POST https://api.juspay.in/card/update \
 -u your_api_key: \
+-H 'x-routing-id: customer_1122'\
 -d card_token=c75a9fca-1121-xxxx-xxxx-47d5c4971456
 -d customer_id=cst_123456789
 -d nickname=suresh
