4f3089af9864179d3416ab4b2397f90eff6ee5a1
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:47:58 +0530

    Update request.sh

--- Wallet/topup/request.sh
+++ Wallet/topup/request.sh
@@ -1,5 +1,6 @@
 curl -X POST https://api.juspay.in/wallets/$wallet_id/topup \
 -u your_api_key: \
+-H 'x-routing-id: customer_1122'\
 -d "amount=1.00"
 -d "topup_txn_id=topup_sdj4kjr8"
 -d "return_url=https://shop.merchant.com/payments/handleResponse"
