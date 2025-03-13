a85fcb91f35b27bb3b3fb829c84cf7167019cd57
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:22:36 +0530

    Update request.sh

--- mandates/exercise/request.sh
+++ mandates/exercise/request.sh
@@ -1,5 +1,6 @@
 curl --request POST \
      --url 'https://api.juspay.in/txns/#Merchant-exercise-the-Mandate' \
      --header 'accept: application/json' \
+     --header 'x-routing-id: customer_1122'\
      --header 'authorization: Basic OUU4QkxxxxxxxxxxxxJDQTQzMEM2RkFDMjcyQjM5Og==' \
      --header 'content-type: application/x-www-form-urlencoded'
