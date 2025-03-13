19eca65f2ac8f76530531e1bca32046e5c480045
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:22:12 +0530

    Update request.sh

--- mandates/check-status/request.sh
+++ mandates/check-status/request.sh
@@ -1,4 +1,5 @@
 curl --location 'https://sandbox.juspay.in/mandates/hyhTD5Ga4jJ2KXZwwnyh4A' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-routing-id: customer_1122'\
 --header 'Authorization: your api key' \
 --data-urlencode 'command=check_status'
