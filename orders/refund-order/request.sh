c7864a596bbc3017ab4534fd8383eed9e9dede7f
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:23:54 +0530

    Update request.sh

--- orders/refund-order/request.sh
+++ orders/refund-order/request.sh
@@ -1,6 +1,7 @@
 curl -X POST https://api.juspay.in/orders/1418394476/refunds \
 -u your_api_key: \
 -H "version:2023-06-30" \
+-H 'x-routing-id: customer_1122'\
 -H 'Content-Type: application/x-www-form-urlencoded'\
 -H 'x-merchantid: merchant_id'\
 -d "unique_request_id=xyz123" \
