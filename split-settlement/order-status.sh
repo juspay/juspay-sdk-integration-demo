0f9db1f28bdc19d0089dfe16f070a59862e20db7
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:38:58 +0530

    Update order-status.sh

--- split-settlement/order-status.sh
+++ split-settlement/order-status.sh
@@ -1,3 +1,4 @@
 curl --location --request GET 'https://api.juspay.in/orders/splitsettlement_1619009418_test' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-routing-id: customer_1122'\
 --header 'Authorization: Basic <base64 of Merchant API Key>'
