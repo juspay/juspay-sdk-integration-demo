f41f1e11d4145eb07a526557de2659f0c3621307
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:15:52 +0530

    Update request-upi.sh

--- mandates/order-status/request-upi.sh
+++ mandates/order-status/request-upi.sh
@@ -1,4 +1,5 @@
 curl GET 'https://api.juspay.in/orders/234823498237423894273498' \
 --header 'x-merchantid: merchant' \
+--header 'x-routing-id: customer_1122'\
 --header 'Authorization: Basic <base64 of key:>'
 --header 'version: 2023-06-30'
