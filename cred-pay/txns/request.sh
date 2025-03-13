a90d24ea0aa8b3219f18b2537766315c28700adb
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:44:28 +0530

    Update request.sh

--- cred-pay/txns/request.sh
+++ cred-pay/txns/request.sh
@@ -1,5 +1,6 @@
 curl --location --request POST 'https://api.juspay.in/txns' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-routing-id: customer_1122'\
 --data-urlencode 'order_id=test_1610009874' \
 --data-urlencode 'merchant_id=Symbol' \
 --data-urlencode 'payment_method_type=WALLET' \
