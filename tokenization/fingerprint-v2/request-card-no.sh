3fe8eb6331fe2a9780f8e3238583b92eeee64105
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:39:08 +0530

    Update request-card-no.sh

--- tokenization/fingerprint-v2/request-card-no.sh
+++ tokenization/fingerprint-v2/request-card-no.sh
@@ -1,5 +1,6 @@
 curl --location 'https://sandbox.juspay.in/v2/card/fingerprint' \
 --header 'Content-Type: application/json' \
+--header 'x-routing-id: customer_1122'\
 --header 'Authorization: Basic <MASKED API KEY>' \
 --data '{
     "card_number": "Card Number",
