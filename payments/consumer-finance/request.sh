0fcdff1cd4c1a1dab4c086ac53547e02ec09f946
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:02:45 +0530

    Update request.sh

--- payments/consumer-finance/request.sh
+++ payments/consumer-finance/request.sh
@@ -2,4 +2,5 @@ curl --request POST \
      --url https://api.juspay.in/txns \
      --header 'accept: application/json' \
      --header 'authorization: Basic OUU4xxxxxEU2NjxxxxxxQTQzMEM2RkFDMjcyQjM5Og==' \
+     --header 'x-routing-id: customer_1122'\
      --header 'content-type: application/x-www-form-urlencoded'
