cf0639a4b3c8ff4186bb59ef3bc7a004a87ee893
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 20:11:24 +0530

    Update getvan.sh

--- van/getvan.sh
+++ van/getvan.sh
@@ -1,4 +1,5 @@
 curl --location 'https://sandbox.juspay.in/customers/:customer_id/virtual-accounts/:virtual_account_reference' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-routing-id: customer_1122'\
 --header 'Authorization: Basic RjMx*******Q0I0NjMyOg=='\
 --header 'x-merchantid: merchant_id'
