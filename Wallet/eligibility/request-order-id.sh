974a30177b48ce518df3d81b616065b87258a400
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:49:17 +0530

    Update request-order-id.sh

--- Wallet/eligibility/request-order-id.sh
+++ Wallet/eligibility/request-order-id.sh
@@ -1,4 +1,5 @@
 curl --location --request POST 'https://api.juspay.in/customers/testwallet145/eligibility' \
 --header 'Authorization: Basic NTlEQzNG****QzE1Og==' \
+--header 'x-routing-id: customer_1122'\
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'order_id=test1234'
