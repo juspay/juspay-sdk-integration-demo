17742270782a4c3dff1466094f31575953bfffef
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:21:53 +0530

    Update request.sh

--- orders/update-order/request.sh
+++ orders/update-order/request.sh
@@ -1,5 +1,6 @@
 curl -X POST https://api.juspay.in/orders/JPAYNEW035 \
 -u your_api_key: \
+-H 'x-routing-id: customer_1122'\
 -H 'Content-Type: application/x-www-form-urlencoded'\
 -H 'x-merchantid: merchant_id'\
 -d "amount=90.00"
