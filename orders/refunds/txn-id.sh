1bf13430174d42f55ffdc52c12248b741fae921b
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:24:58 +0530

    Update txn-id.sh

--- orders/refunds/txn-id.sh
+++ orders/refunds/txn-id.sh
@@ -1,5 +1,6 @@
 curl --location --request POST 'https://api.juspay.in/refunds' \
 --header 'Authorization: Basic NTlE*****E4QzE1Og==' \
+--header 'x-routing-id: customer_1122'\
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'unique_request_id=refund_122_3472' \
 --data-urlencode 'amount=1' \
