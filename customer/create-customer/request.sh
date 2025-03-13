0e59329bb4da5ae2ec4b93f9fcdf2a05f892a937
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:18:26 +0530

    Update request.sh

--- customer/create-customer/request.sh
+++ customer/create-customer/request.sh
@@ -1,6 +1,7 @@
 curl -X POST https://api.juspay.in/customers\
 -u your_api_key:\
 -H 'x-merchantid: merchant_id'\
+-H 'x-routing-id: customer_1122'\
 -H 'Content-Type: application/x-www-form-urlencoded'\
 -d "object_reference_id=customer@gmail.com"\
 -d "mobile_number=9988776655"\    
