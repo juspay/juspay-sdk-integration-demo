e68ad6c2109e4ccbf6f4a05cd57652f81036a9bb
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:25:20 +0530

    Update instant.sh

--- orders/refunds/instant.sh
+++ orders/refunds/instant.sh
@@ -2,4 +2,5 @@ curl -X POST \
  https://api.juspay.in/refunds \
  -u your_api_key: \
  -H 'Content-Type: application/x-www-form-urlencoded' \
+ -H 'x-routing-id: customer_1122'\
  -d 'unique_request_id=rfd_1234&amount=98.20&order_id=ord_123&refund_type=INSTANT'
