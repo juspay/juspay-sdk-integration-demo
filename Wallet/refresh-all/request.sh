0c5cb4350a697a4ad12ade94e1f17a943104275c
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 17:50:06 +0530

    Update request.sh

--- Wallet/refresh-all/request.sh
+++ Wallet/refresh-all/request.sh
@@ -1,3 +1,4 @@
 curl --location --request GET 'https://api.juspay.in/customers/cth_eN4e4kR12CJV/wallets/refresh-balances' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'x-routing-id: customer_1122'\
 --header 'Authorization: Basic NTlEQ****QzE1Og=='
