4340836881e8fd7540c64e99b10c67d96db5d2cd
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:49:36 +0530

    Update deleteRq.sh

--- tokenization/deleteAPI/deleteRq.sh
+++ tokenization/deleteAPI/deleteRq.sh
@@ -1,4 +1,5 @@
 curl --location --request POST 'https://sandbox.juspay.in/v4/card/delete' \
 --header 'Authorization: Basic <Base 64 encoded of API key>' \
+--header 'x-routing-id: customer_1122'\
 --header 'Content-Type: application/json' \
 --data-raw '{"card_reference":"card_reference_values_goes_here","customer_id": "customer_id_value_goes_here"}'
