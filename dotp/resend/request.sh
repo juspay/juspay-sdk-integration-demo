423bd9f4dcaca83fdfebfafd7d3e07305c339007
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:46:53 +0530

    Update request.sh

--- dotp/resend/request.sh
+++ dotp/resend/request.sh
@@ -1,1 +1,3 @@
-curl -X POST https://api.juspay.in/v2/txns/txn_36dc6eada1d0463da91dc56c8262a8c3/authenticate/ch_xyz/resend -u your_api_key:
+curl -X POST https://api.juspay.in/v2/txns/txn_36dc6eada1d0463da91dc56c8262a8c3/authenticate/ch_xyz/resend \
+-u your_api_key: \
+-H 'x-routing-id: customer_1122'
