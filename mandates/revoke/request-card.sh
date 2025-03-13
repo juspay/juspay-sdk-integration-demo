da4c9a8dd4aa865b4b80b12e0c18860f8b89d5ba
rubaijuspay99 <131948196+rubaijuspay99@users.noreply.github.com>
2025-03-13 18:19:44 +0530

    Update request-card.sh

--- mandates/revoke/request-card.sh
+++ mandates/revoke/request-card.sh
@@ -1,4 +1,5 @@
 curl POST 'https://api.juspay.in/mandates/:mandate_id' \
     -H 'Authorization: Basic <Base-64 Key>' \
+    -H 'x-routing-id: customer_1122'\
     -H 'x-merchantid: merchant' \
 	-d command=”revoke”
