758f6005581df3882c95ee03dc8d8be7ebfec76c
Laya Hilarius <laya.hilarius@juspay.in>
2025-04-28 14:27:43 +0530

    Juspay Brazil Frontend Doc Changes

---
+++ br/payment-page-android-kotlin-integration-demo/app/src/main/java/in/juspay/devtools/MainActivity.kt
@@ -0,0 +1,34 @@
+package `in`.juspay.devtools
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    var SPLASH_SCREEN_TIME_OUT = 500
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+        setContentView(R.layout.activity_main)
+        //this will bind your MainActivity.class file with activity_main.
+        Handler().postDelayed({
+            val i = Intent(
+                this@MainActivity,
+                ProductsActivity::class.java
+            )
+            //Intent is used to switch from one activity to another.
+            startActivity(i)
+            //invoke the SecondActivity.
+            finish()
+            //the current activity will get finished.
+        }, SPLASH_SCREEN_TIME_OUT.toLong())
+    }
+}
