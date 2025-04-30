c076aee0cb28db1670b7b4d34934e7d1104707a8
srikanthjuspay <152646296+srikanthjuspay@users.noreply.github.com>
2025-04-30 16:48:47 +0530

    Update request.sh
    Updating amount param

--- payments/emi/request.sh
+++ payments/emi/request.sh
@@ -3,6 +3,6 @@ curl --location 'https://smartgateway.hdfcbank.com/v2/emi/plans' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: Basic <base64 of Merchant API Key>' \
 --data-urlencode 'merchant_id=merchant_id' \
---data-urlencode 'order_amount=1200' \
+--data-urlencode 'order_amount=1500' \
 --data-urlencode 'mobile_number=9999999999' \
---data-urlencode 'order_basket=[{"id":"491838009","description":"Test Product","quantity":1,"unitPrice":25123.25,"category":"Electronics","sku":"12345","productUrl":"https://www.google.com","sellerType":"VENDOR","customParams":{"name1":"value1","name2":"value2"}}]'
+--data-urlencode 'order_basket=[{"id":"491838009","description":"Test Product","quantity":1,"unitPrice":1500,"category":"Electronics","sku":"12345","productUrl":"https://www.google.com","sellerType":"VENDOR","customParams":{"name1":"value1","name2":"value2"}}]'
