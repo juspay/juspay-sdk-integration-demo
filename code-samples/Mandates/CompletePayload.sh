c1a652de49b476335e490f9c5805408f7af96777
srikanthjuspay <152646296+srikanthjuspay@users.noreply.github.com>
2025-05-16 08:13:04 +0530

    Hdfc sample codes (#875)
    * Create CompletePayload.sh
    
    * Create OTM.sh

---
+++ code-samples/Mandates/CompletePayload.sh
@@ -0,0 +1,24 @@
+curl --location --request POST 'https://smartgateway.hdfcbank.com/session' \
+--header 'Content-Type: application/json' \
+--header 'x-customerid: CUST001' \
+--header 'merchant_id: testhdfc1' \
+--header 'Authorization: Basic <Base64 API key>' \
+--data-raw '{
+    "order_id": "T_9110234219",
+    "amount": "1.00",
+    "customer_id": "C_AYUSH",
+    "payment_page_client_id": "hdfcmaster",
+    "action": "paymentPage",
+    "description": "Complete your payment",
+    "first_name": "Srikanth",
+    "last_name": "Mitra",
+    "options.create_mandate": "REQUIRED",
+    "mandate.start_date": "1747362095",
+    "mandate.rule_value": "15",
+    "mandate.amount_rule" : "FIXED",
+    "mandate.max_amount": "3.00",
+    "mandate.revokable_by_customer": true,
+    "mandate.block_funds": false,
+    "mandate.frequency": "FORTNIGHTLY",
+    "mandate.end_date": "1747534895"
+}'
---
+++ code-samples/Mandates/OTM.sh
@@ -0,0 +1,21 @@
+curl --location --request POST 'https://smartgateway.hdfcbank.com/session' \
+--header 'Content-Type: application/json' \
+--header 'x-customerid: testhdfc1' \
+--header 'merchant_id: 44910' \
+--header 'Authorization: Basic <Base64 API Key>' \
+--data-raw '{
+    "order_id": "T_9110234219",
+    "amount": "1.00",
+    "customer_id": "C_AYUSH",
+    "payment_page_client_id": "hdfcmaster",
+    "action": "paymentPage",
+    "description": "Complete your payment",
+    "first_name": "Srikanth",
+    "last_name": "Mitra",
+    "options.create_mandate": "REQUIRED",
+    "mandate.max_amount": "3.00",
+    "mandate.revokable_by_customer": true,
+    "mandate.block_funds": false,
+    "mandate.frequency": "ONETIME",
+    "mandate.end_date": "1747534895"
+}'
