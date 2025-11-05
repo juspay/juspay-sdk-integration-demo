d0f7cefc9090a137602626e57a0fb115ce857c8b
kdebdeepa <163527548+kdebdeepa@users.noreply.github.com>
2025-11-05 18:56:33 +0530

    Update UPI request URL to production endpoint

--- mandates/list-mandate/request-upi.sh
+++ mandates/list-mandate/request-upi.sh
@@ -1,3 +1,3 @@
-curl --location 'https://smartgatewayuat.hdfcbank.com/customers/<customer_id>/mandates' \
+curl --location 'https://smartgateway.hdfc.bank.in/customers/<customer_id>/mandates' \
 --header 'x-merchant-id: <merchant_id>' \
 --header 'Authorization: Basic <api key in base64 format>' 
