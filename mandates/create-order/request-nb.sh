2665ea25f322997eadfe5c4f9176a8f00bdf7716
kdebdeepa <163527548+kdebdeepa@users.noreply.github.com>
2025-11-05 18:49:43 +0530

    Update API endpoint for request-nb.sh

--- mandates/create-order/request-nb.sh
+++ mandates/create-order/request-nb.sh
@@ -1,4 +1,4 @@
-curl POST \ https://smartgatewayuat.hdfcbank.com/txns \
+curl POST \ https://smartgateway.hdfc.bank.in/txns \
 -H 'x-merchantid: merchant' \
 -H 'Content-Type: application/x-www-form-urlencoded'\
  -d "order_id=152664118690577-910" \
