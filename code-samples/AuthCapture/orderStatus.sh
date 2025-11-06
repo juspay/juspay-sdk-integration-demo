c0ad1ad57e521ff1afab84e3a8b5ebad6897a030
divyashreeg-juspay <divyashree.g@juspay.in>
2025-11-06 14:37:22 +0530

    Update orderStatus.sh

--- code-samples/AuthCapture/orderStatus.sh
+++ code-samples/AuthCapture/orderStatus.sh
@@ -1,4 +1,4 @@
-curl --location --request GET 'https://smartgateway.hdfcbank.com/orders/:orderID' \
+curl --location --request GET 'https://smartgateway.hdfc.bank.in/orders/:orderID' \
 --header 'version: 2024-06-30' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'x-merchantid: testhdfc1' \
