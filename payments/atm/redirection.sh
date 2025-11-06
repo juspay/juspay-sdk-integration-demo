ae683d130e76eb92fecee9705d37e545572402df
divyashreeg-juspay <divyashree.g@juspay.in>
2025-11-06 15:35:20 +0530

    Update redirection.sh

--- payments/atm/redirection.sh
+++ payments/atm/redirection.sh
@@ -1,4 +1,4 @@
-curl -X POST https://smartgateway.hdfcbank.com/txns \
+curl -X POST https://smartgateway.hdfc.bank.in/txns \
 -d "order_id=:order_id" \
 -d "merchant_id=:merchant_id" \
 -d "payment_method_type=CARD" \
