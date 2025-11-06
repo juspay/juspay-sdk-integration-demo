c41d4fd3427b0f47698ab113fd732656fd9a3986
divyashreeg-juspay <divyashree.g@juspay.in>
2025-11-06 17:46:59 +0530

    Update redirection.json (#909)
    * Update redirection.json
    
    * Update redirection.sh
    
    * Update seamless.json
    
    * Update seamless.sh
    
    * Update new-card-response.json
    
    * Update new-card.json
    
    * Update saved-card-response.json
    
    * Update saved-card.json
    
    * Update request.sh
    
    * Update response.json
    
    * Update get-response.json
    
    * Update request.sh
    
    * Update get-response.json

--- payments/atm/seamless.sh
+++ payments/atm/seamless.sh
@@ -1,5 +1,5 @@
 // A regular card transaction
-curl -X POST https://smartgateway.hdfcbank.com/txns \
+curl -X POST https://smartgateway.hdfc.bank.in/txns \
 -d "order_id=:order_id" \
 -d "merchant_id=:merchant_id" \
 -d "payment_method_type=CARD" \
