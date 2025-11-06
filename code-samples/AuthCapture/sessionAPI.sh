36c72547e0003bc53a7b3b15d3455da4da27cdbe
divyashreeg-juspay <divyashree.g@juspay.in>
2025-11-06 14:37:53 +0530

    Update sessionAPI.sh

--- code-samples/AuthCapture/sessionAPI.sh
+++ code-samples/AuthCapture/sessionAPI.sh
@@ -1,4 +1,4 @@
-curl --location --request POST 'https://smartgateway.hdfcbank.com/session' \
+curl --location --request POST 'https://smartgateway.hdfc.bank.in/session' \
 --header 'x-merchantid: 34436' \
 --header 'Content-Type: application/json' \
 --header 'version: 2024-05-01' \
