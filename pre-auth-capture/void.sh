9ed561d45f7a3168818e8c107693d26d615d1bb1
divyashreeg-juspay <divyashree.g@juspay.in>
2025-11-06 12:37:29 +0530

    Update capture.json (#902)
    * Update capture.json
    
    * Update capture.sh
    
    * Update void.sh to use new HDFC bank URL
    
    * Update request.sh
    
    * Update payment URL in response.json

--- pre-auth-capture/void.sh
+++ pre-auth-capture/void.sh
@@ -1,4 +1,4 @@
 curl --request POST \
-     --url https://smartgateway.hdfcbank.com/v2/txns/txn_uuid/void \
+     --url https://smartgateway.hdfc.bank.in/v2/txns/txn_uuid/void \
      --header 'accept: application/json' \
      --header 'authorization: Basic OUU4QxxxxxxxxxxxxxxxxTQzMEM2RkFDMjcyQjM5Og=='
