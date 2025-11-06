eaa567f0f22056e6048ea9b8f53ea36bdb111981
divyashreeg-juspay <divyashree.g@juspay.in>
2025-11-06 15:09:36 +0530

    Update capture.sh

--- code-samples/AuthCapture/capture.sh
+++ code-samples/AuthCapture/capture.sh
@@ -1,3 +1,3 @@
-curl --location --request POST 'https://smartgateway.hdfcbank.com/v2/txns/:txnUUID/capture' \
+curl --location --request POST 'https://smartgateway.hdfc.bank.in/v2/txns/:txnUUID/capture' \
 --header 'accept: application/json' \
 --header 'Authorization: Basic <Base64EncodedAPIKEY>'
