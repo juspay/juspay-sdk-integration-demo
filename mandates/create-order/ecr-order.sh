d86e48205ce37e0f4eb1ca02107b559b97a34426
divyashreeg-juspay <divyashree.g@juspay.in>
2025-11-06 14:22:04 +0530

    Update ecr-order.sh

--- mandates/create-order/ecr-order.sh
+++ mandates/create-order/ecr-order.sh
@@ -1,4 +1,4 @@
-curl --location --request POST 'https://smartgatewayuat.hdfcbank.com/session' \
+curl --location --request POST 'https://smartgateway.hdfcuat.bank.in/session' \
 --header 'Content-Type: application/json' \
 --header 'Authorization: Basic base_64_encoded_api_key==' \
 --header 'version: 2023-08-01' \
