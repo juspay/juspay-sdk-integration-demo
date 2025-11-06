c125ee1c7baeacee35dfa7bfb107329336401d80
divyashreeg-juspay <divyashree.g@juspay.in>
2025-11-06 12:39:17 +0530

    Update request-encrypted.sh

--- code-samples/sample-response/refunds/request-encrypted.sh
+++ code-samples/sample-response/refunds/request-encrypted.sh
@@ -1,4 +1,4 @@
-curl --location --globoff 'https://smartgatewayuat.hdfcbank.com/orders/{order_id}/refunds' \
+curl --location --globoff 'https://smartgateway.hdfcuat.bank.in/orders/{order_id}/refunds' \
 --header 'x-merchantid: merchant_id' \
 --header 'x-customerid: customer_id' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
