e361b81dc3e9deb0776eee300a53de1b81d08422
divyashreeg-juspay <divyashree.g@juspay.in>
2025-11-06 15:17:29 +0530

    Update TranTPVResp.sh

--- TPV/TranTPVResp.sh
+++ TPV/TranTPVResp.sh
@@ -4,9 +4,9 @@
     "id": "ordeh_9d1cbcbcc0ad41058df6c14384ce155c",
     "order_id": "TPVTEST14",
     "payment_links": {
-        "mobile": "https://smartgatewayuat.hdfcbank.com/payment-page/order/ordeh_9d1cbcbcc0ad41058df6c14384ce155c",
-        "web": "https://smartgatewayuat.hdfcbank.com/payment-page/order/ordeh_9d1cbcbcc0ad41058df6c14384ce155c",
-        "iframe": "https://smartgatewayuat.hdfcbank.com/payment-page/order/ordeh_9d1cbcbcc0ad41058df6c14384ce155c"
+        "mobile": "https://smartgateway.hdfcuat.bank.in/payment-page/order/ordeh_9d1cbcbcc0ad41058df6c14384ce155c",
+        "web": "https://smartgateway.hdfcuat.bank.in/payment-page/order/ordeh_9d1cbcbcc0ad41058df6c14384ce155c",
+        "iframe": "https://smartgateway.hdfcuat.bank.in/payment-page/order/ordeh_9d1cbcbcc0ad41058df6c14384ce155c"
     },
     "order_expiry": "2024-12-18T05:42:58Z"
 }
