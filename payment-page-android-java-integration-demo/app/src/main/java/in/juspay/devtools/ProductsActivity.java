6ec35a59d0b4b10d5861a02edc7d58ff930bd7fc
Satvik Sawhney <satvik.sawhney@juspay.in>
2025-12-15 17:11:53 +0530

    Added x-routing-id for Java and Kotlin (android)

--- payment-page-android-java-integration-demo/app/src/main/java/in/juspay/devtools/ProductsActivity.java
+++ payment-page-android-java-integration-demo/app/src/main/java/in/juspay/devtools/ProductsActivity.java
@@ -79,8 +79,9 @@ public class ProductsActivity extends AppCompatActivity {
         try {
             // generating inner payload
             innerPayload.put("action", "initiate");
-            innerPayload.put("merchantId", "<MERCHANT_ID>");    // Put your Merchant ID here
+            innerPayload.put("merchantId", "<MERCHANT_ID>");      // Put your Merchant ID here
             innerPayload.put("clientId", "<CLIENT_ID>");          // Put your Client ID here
+            innerPayload.put("xRoutingId", "<X_ROUTING_ID>");     // Put your X-Routing ID here
             innerPayload.put("environment", "production");
             sdkPayload.put("requestId",  ""+ UUID.randomUUID());
             sdkPayload.put("service", "in.juspay.hyperpay");
