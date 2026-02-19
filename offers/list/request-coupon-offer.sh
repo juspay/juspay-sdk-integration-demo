aab88313c1486d8e32299a31ce19c35f11672464
kdebdeepa <163527548+kdebdeepa@users.noreply.github.com>
2026-02-19 22:27:18 +0530

    Update API endpoint for coupon offers request

--- offers/list/request-coupon-offer.sh
+++ offers/list/request-coupon-offer.sh
@@ -1,4 +1,4 @@
-curl --location --request POST 'https://api.juspay.in/v1/offers/list' \
+curl --location --request POST 'https://smartgateway.hdfc.bank.in/v1/offers/list' \
 --header 'Authorization: <API KEY>' \
 --header 'Content-Type: application/json' \
 --header 'x-routing-id: customer_1122'\
