66ed43dc331acb79d3ded105cf0e76975bf7acee
kdebdeepa <163527548+kdebdeepa@users.noreply.github.com>
2026-02-23 15:29:21 +0530

    Update API endpoint in request.sh
    For offers

--- offers/status/request.sh
+++ offers/status/request.sh
@@ -1,4 +1,4 @@
-curl --location --request GET 'https://api.juspay.in/orders/JP1636474794' \
+curl --location --request GET 'https://smartgateway.hdfcuat.bank.in/orders/JP1636474794' \
 --header 'version: 2023-06-30' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'x-routing-id: customer_1122'\
