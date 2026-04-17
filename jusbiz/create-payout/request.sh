e7c56aca1515cb3f12b2125833f5f32a8fccb301
vivek vikram <vivek.vikram@vivekvikram-KT4VGFW651.local>
2026-04-17 16:35:23 +0530

    jusbiz-x-merchant-id

--- jusbiz/create-payout/request.sh
+++ jusbiz/create-payout/request.sh
@@ -1,7 +1,7 @@
 curl --location 'https://sandbox.biz.juspay.in/ardra/vt/provision/v2' \
 --header 'KeyId: <key Id>>' \
 --header 'Content-Type: application/json' \
---header 'X-merchantid: <merchant id>' \
+--header 'x-merchant-id: <merchant id>' \
 --header 'Authorization: Basic (b64 encoded API key)' \
 --data '{
     "payoutId": "Juspay123456789",
--- jusbiz/token-status-check/request.sh
+++ jusbiz/token-status-check/request.sh
@@ -1,6 +1,6 @@
 curl --location 'https://sandbox.biz.juspay.in/ardra/vt/fetchStatus/v2' \
 --header 'KeyId: <key Id>' \
---header 'X-merchantid: <merchant id>' \
+--header 'x-merchant-id: <merchant id>' \
 --header 'Content-Type: application/json' \
 --header 'Authorization: Basic MktCUUJaVVJMQkU3WVVHUjo=' \
 --data '{
