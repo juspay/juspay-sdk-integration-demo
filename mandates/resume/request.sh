0569ed498d122483ae1971522d36af20ba65e45d
divyashreeg-juspay <divyashree.g@juspay.in>
2026-04-22 16:27:22 +0530

    Update request.sh

--- mandates/resume/request.sh
+++ mandates/resume/request.sh
@@ -1,5 +1,5 @@
 curl --location --request POST 'http://smartgateway.hdfcuat.bank.in/mandates/7KKT3yu3LcQjskY5F4moS5M' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
---header 'Authorization: Basic QjU3Qxxxxxxxxyuegj2ehkQ0MTg4N0NEMDU6' \
+--header 'Authorization: Basic <api key in base64 format>' \
 --data-urlencode 'command=resume' \
 --data-urlencode 'resume_date=1696032000'
