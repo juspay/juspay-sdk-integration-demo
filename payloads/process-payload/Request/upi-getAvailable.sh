d78bc524207b36ff1c78093b1113a2eeaa7498f7
sekharpidugu-hyperpg <sekhar.pidugu@juspay.in>
2026-07-22 17:15:15 +0530

    Update upi-getAvailable.sh

--- payloads/process-payload/Request/upi-getAvailable.sh
+++ payloads/process-payload/Request/upi-getAvailable.sh
@@ -1,6 +1,6 @@
                     {
   "requestId": "6fd3bb9a-4583-42f5-a16c-71109906e1f3",
-  "service": "in.juspay.hyperapi",
+  "service": "hyperapi",
   "payload": {
     "action": "upiTxn",
     "getAvailableApps": true,
