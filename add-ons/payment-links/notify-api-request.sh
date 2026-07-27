ac53c37def88bd776eeb390967ad794700d60975
divyashreeg-juspay <divyashree.g@juspay.in>
2026-07-27 17:19:50 +0530

    Add notify-api-request.sh for payment link notifications (#984)
    * Add notify-api-request.sh for payment link notifications
    
    * Add Notify API response JSON file
    
    * Add notify-api_response.json for payment links

---
+++ add-ons/payment-links/notify-api-request.sh
@@ -0,0 +1,13 @@
+curl --location 'https://smartgateway.hdfc.bank.in/paymentLink/notify' \
+--header 'Authorization: BASE 64 Encoded API KEY' \
+--header 'User-Agent: curl/7.64.1' \
+--header 'X-Auth-Scope: scope_value' \
+--header 'X-Forwarded-For: 203.0.113.42' \
+--header 'x-merchantid: merchant_id' \
+--header 'Content-Type: application/json' \
+--data '{
+    "send_mail" : true,
+    "send_sms" : true,
+    "send_whatsapp" : false,
+    "order_id" : "TestingOrderID"
+}'
